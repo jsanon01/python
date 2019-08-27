b752bfe5cc3b4030ec6ecfe9235b43123871a526
jean sanon <karlo@Pascals-iMac.local>
2019-08-26 21:13:29 -0400

    Initial commit

---
+++ colors.py
@@ -0,0 +1,13 @@
+alien_color = input("Enter a color: ")
+
+alien = 'green'
+
+if alien == 'green':
+	print("The player earns 5 points")
+elif alien == 'yellow':
+	print("The player earns 10 points")
+elif alien == 'red':
+	print("The player earns 15 points")
+else:
+	print("No color did not match")
+
