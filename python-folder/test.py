7d2d73b683e6748881fac205dfc29cfc5327a810
jean sanon <karlo@Pascals-iMac.local>
2019-08-28 21:59:44 -0400

    python-folder/

---
+++ python-folder/test.py
@@ -0,0 +1,23 @@
+
+# name = input("Here are the list of car names: ")
+
+# cars = ['audi','suzuki', 'bmw', 'kia', 'toyota']
+
+# print(cars)
+# for car in cars:
+# 	if car == 'bmw':
+# 	    print(car.upper())
+# 	else:
+# 		print(car.title())
+
+# txt = input("Enter some text: ")
+
+# print("Is that what you said.")
+
+text = int(input('Enter a number: '))
+
+print('The number you entered is ' + text)
+
+name = input('Enter a name: ')
+
+print('The name you entered is ' + name)
