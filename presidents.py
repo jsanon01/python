32b99d75e97717d9ddbe916fdc1e3b28595eff72
jean sanon <karlo@Pascals-iMac.local>
2019-09-26 21:38:47 -0400

    Initial commit

---
+++ presidents.py
@@ -0,0 +1,8 @@
+# List of 20th and 21st century US Presidents
+
+presidents = ['jfk', 'clinton', 'obama', 'trump']
+
+for president in presidents:
+    print(f'{president.title()} was elected US President!')
+print('They are all Americans-born!')
+
