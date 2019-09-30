109ee77273339ff483185d76be2a4d60553229c0
JBS <jeansanon180@gmail.com>
2019-09-29 20:04:10 -0400

    Initial commit

---
+++ python-folder/add-remove-items.py
@@ -0,0 +1,35 @@
+# Adding and Removing names/items
+
+names = ['petion', 'bob', 'david', 'alice', 'carolina', 'johny', 'pascal']
+
+names.remove('petion') # removing petion from list 
+
+names.remove('david') # removing david from list 
+
+names.append('dessalines') # appending = adding to list 
+
+names.append('estime') # appending = adding to list 
+
+names.sort()
+
+# names.title()
+
+# names.reverse() # reverse the list order
+
+for name in names:
+    print(f'{name.title()} is a human name.')
+
+#print(f'{name.title()} are human names.')
+print(f'They are all human namnes.')
+
+# find out if alice is in names
+# if alice in names print 'Yes Alice is there'
+# if not print 'No Alice is not there'
+# names = ['petion', 'bob', 'david', 'alice', 'carolina', 'johny', 'pascal']
+# name = 'karlo'
+
+# if name in names:
+#     print(f'{name} is found')
+# else:
+#     print(f'{name} is not found')
+
