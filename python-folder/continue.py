96664a1682b6af10b87a8d0c741a60f48e6b2e43
JBS <jeansanon180@gmail.com>
2019-10-01 21:42:57 -0400

    Initial commit

---
+++ python-folder/continue.py
@@ -0,0 +1,12 @@
+# Break statement
+
+number = 0
+
+print('Statement will BREAK at 7')
+
+for number in range(0, 10):
+    if number == 7:
+        continue; # staement will continue
+    else:
+        print(f'Number is ' + str(number))
+
