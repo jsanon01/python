a814789419a0d8b17db7aa09850d8867d7b70a75
JBS <jeansanon180@gmail.com>
2019-10-03 09:20:34 -0400

    2nd commit

--- python-folder/class-dog.py
+++ python-folder/class-dog.py
@@ -1,14 +1,15 @@
 # Class Dog
 
-class Dog:
+class Dog: # defining dog's class and properties
     def __init__(self, color, name, age, race):
         self.name = name
         self.age = age
         self.color = color
         self.race = race
 
+# defining 3 functions aloowing 3 dog's activities
     def sit(self):
-        print(f'{self.name} is  now sitting down')
+        print(f'{self.name} can sit down when asked')
 
     def roll_over(self):
         print(f'{self.name} will roll over')
@@ -16,13 +17,16 @@ class Dog:
     def bark(self):
         print(f'{self.name} barks loudly!')
 
+# making 2 instances from dog's class
 my_dog = Dog('golden', 'Willie', 6, 'German shepherd')
 
+# printing dog's class and properties
 print(f'My dog name is  {my_dog.name}.')
-print(f'My dog race is {my_dog.race}.')
-print(f'My dog age is {my_dog.age} years old.')
-print(f'My dog color is {my_dog.color}!')
+print(f'{my_dog.name} race is {my_dog.race}.')
+print(f'{my_dog.name} age is {my_dog.age} years old.')
+print(f'{my_dog.name} color is {my_dog.color}!')
 
+# calling the pre-assigned functions
 my_dog.sit()
 my_dog.roll_over()
 my_dog.bark()
