31a73796652a8e5bbddbff0653bbaee68c0b4a49
JBS <jeansanon180@gmail.com>
2019-10-02 21:55:17 -0400

    initial commit

---
+++ python-folder/function.py
@@ -0,0 +1,24 @@
+
+def function():
+    print('Hello from a Function')
+
+function() # calling function
+
+print('---------------')
+
+def my_function(name):
+    print(name + ' Christophe')
+
+my_function('Kedner')
+my_function('Fritzner')
+my_function('Joel')
+my_function('Micardo')
+
+print('===============')
+
+def origin(country = 'Haiti'):
+    print("I'm from " + country)
+
+origin('Africa')
+origin('Jamaica')
+origin()
