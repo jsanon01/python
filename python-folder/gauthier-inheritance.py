9582788b17a4ae28b34eb43337363fde5180dae7
JBS <jeansanon180@gmail.com>
2019-10-04 21:31:44 -0400

    Initial commit

---
+++ python-folder/gauthier-inheritance.py
@@ -0,0 +1,33 @@
+# Script is about Inheritance
+# 1 class + 2 functions (names | print)
+# calling class + printname
+
+class Gauthier:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+x = Gauthier('Ben', 'Gauthier')
+x.printname()
+
+y = Gauthier('Franckel', 'Gauthier')
+y.printname()
+
+z = Gauthier('Ducarte', 'Gauthier')
+z.printname()
+        
+class Cousin(Gauthier):
+    pass
+
+print('---Cousins and Siblings---')
+a = Cousin('Famille', 'Auguste')
+a.printname()
+
+b = Cousin('Famille', 'Josma')
+b.printname()
+
+c  = Cousin('Famille', 'Joseph')
+c.printname()
