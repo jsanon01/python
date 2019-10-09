3e84028386ef4b6680a70c03b76b20dcebe3f9a5
JBS <jeansanon180@gmail.com>
2019-10-08 22:12:52 -0400

    Initial commit

---
+++ python-folder/favorite-languages.py
@@ -0,0 +1,11 @@
+
+favorite_languages = {
+			'jerry mazouka': 'python',
+			'king kino': 'java',
+			'celestin': 'C #',
+			'roberto martino': 'javascript',
+			'charles baker': 'nodejs'
+			}
+
+for k, v in favorite_languages.items():
+    print(f"{k.title()}'s favorite language is {v.title()}.")
