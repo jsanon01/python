8d5a28642dc49d4e3a0ceb217cc8f30661b88452
JBS <jeansanon180@gmail.com>
2019-10-10 22:13:54 -0400

    1st commit

---
+++ python-folder/for.py
@@ -0,0 +1,9 @@
+# This script is about For statement
+# which is a collection-controlled loop
+
+words = ['phillip madere', 'henri christophe ', 'kelo cadet','jean-jacques dessalines', 'franklin pierre']
+
+print('Here are some words count: ')
+
+for name in words:
+    print(name.title(), len(name))
