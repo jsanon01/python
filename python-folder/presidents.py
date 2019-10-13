2f99d795efd2cea5a307a04c055d5de8954b4a30
JBS <jeansanon180@gmail.com>
2019-10-12 22:32:29 -0400

    1st commit

---
+++ python-folder/presidents.py
@@ -0,0 +1,12 @@
+# List of 20th and 21st century US Presidents
+
+presidents = ['john kennedy','jimmy carter', 'bill clinton', 'barak obama', 'donald trump']
+
+us = 'was elected US President!'
+
+for president in presidents:
+    print(f'{president.title()} '+ us )
+    #print(f'{president.title()} was elected US President!')
+print('They are all Americans-born!')
+
+
