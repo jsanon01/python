f986c6f4f54bd652e001700682a482081567fd19
JBS <jeansanon180@gmail.com>
2019-10-14 21:57:42 -0400

    1st commit

---
+++ python-folder/rivers.py
@@ -0,0 +1,9 @@
+#This script not only prints list but aslo uses uppercase function
+
+
+rivers = ['marfranc', 'moron', 'abricots','chambellan', 'voldrogue', 'jeremie']
+
+rivers.sort()
+
+for river in rivers:
+        print(f'{river.title()} is in Grand-Anse Department')
