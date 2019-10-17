7ef06bd90cccb65e2fe5e798fd52b3843acddeda
JBS <jeansanon180@gmail.com>
2019-10-16 22:29:29 -0400

    1st commit

---
+++ python-folder/juridiction-ga.py
@@ -0,0 +1,17 @@
+# Script related to Grand-Anse Department
+# with keyword as function
+
+def keyword(comm_1, comm_2, comm_3, comm_4, comm_5, comm_6, comm_7):
+    print('Grand-Anse is divided in some communes:')
+    print(comm_1 + ' is located between ' + comm_7 +' and ' + comm_4)
+    print(comm_2 + ' is located between ' + comm_3 + ' and '+ comm_4)
+    print(comm_3 + ' is located between ' + comm_7 + ' and '+ comm_2)
+    print(comm_4 + ' is located between ' + comm_2 + ' and '+ comm_3)
+    print(comm_5 + ' is located between ' + comm_1 + ' and '+ comm_2)
+    print(comm_6 + ' is located between ' + comm_5 + ' and  Roseaux')
+
+
+keyword(comm_1 = 'Jeremie', comm_2 = 'Anse-Dhailnaut', comm_3 = 'Dame-Marie', comm_4 = 'Abricots', comm_5 = 'Beaumont', comm_6 = 'Pestel', comm_7 = 'Marfranc')
+
+#print(keyword)
+
