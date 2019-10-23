e6dedcb4ed7ebda3165caa13e461e35a224b8a34
JBS <jeansanon180@gmail.com>
2019-10-22 21:53:19 -0400

    1st commit

---
+++ python-folder/name.py
@@ -0,0 +1,16 @@
+# Script is about first & last names
+
+fname = input('Enter first name: ')
+
+lname = input('Enter last name: ')
+
+name = 'eric'
+
+name = 'Eric'
+
+if name == 'eric' or name == 'Eric':
+    print("You are Haitian")
+else:
+    print('It is a French name')
+
+print(f"Your first name is {fname.title()} and last name is {lname.title()}")
