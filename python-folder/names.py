4401014769e5b2609840c721ac10384b38357695
JBS <jeansanon180@gmail.com>
2019-10-22 21:54:19 -0400

    1st commit

---
+++ python-folder/names.py
@@ -0,0 +1,31 @@
+# This script combines 3 snippets
+# 1) it prints names using title function
+
+# print full name (first + last)
+print('--------------Printing First & Last Names----------------')
+first_name = 'ana'
+
+last_name = 'pierre'
+
+full_name = f'{first_name} {last_name}'
+
+print(full_name.title())
+
+# 2) same script (full_name.py) prints names location
+print('--------------Printing Location Names----------------')
+first = 'martissant'
+
+last = 'carrefour'
+
+names = f"{first} {last}"
+
+print(names.title())
+
+# 3) it prints apostrophe.py
+print('--------------Printing Apostrophe Usage----------------')
+message = "One of Python's strengths is its diversity."
+print(message)
+messages = 'One of Python\'s strengths is its diversity.'
+print(messages)
+messaj = 'Haiti est independant depuis 1804!'
+print(messaj)
