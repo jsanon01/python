4784b1e0d794a87c15c834252f79bcaff4bba712
jean sanon <jeansanon180@gmail.com>
2019-11-04 16:59:07 -0500

    Initial commit

---
+++ python-folder/big-mama.py
@@ -0,0 +1,24 @@
+# This script prints a keys-values of a dictionary
+
+list = {
+	'guys': "when a woman loves you, the world is not against you...",
+	'biologically': "speaking, man gives and woman receives...",
+	'physiologically': "speaking, man stands up for urinate whereas woman sits...",
+	'do not': "open legs for the sake of opening (with no will)...",
+	'all': "birds look the same without feathers...",
+	'pussy': "debt never gets paid...",
+	'sexual': "unfinished business never ends...",
+	'women': "get married when being asked...",
+	'can': "sex shift you up to the nex level? Certainly Yes...",
+	'sex': "is natural phenomenal social,thus, it crosses societies...",
+	"'crab metaphor:'": "if i can not have it, neither can you!",
+	'ladies and gentlemen': "never sleep where you should not stand nor walk...",
+
+	}
+
+print(f'\n------ Big Mama: the 12 Fundamental Principles -------' )
+
+for k, v in list.items():
+    print(f'\n{k.title()} {v}')
+
+print()
