e12673cd9aa84fb957eb91bba70de2a993fb6476
jean sanon <jeansanon180@gmail.com>
2019-11-09 22:52:27 -0500

    Initial commit

---
+++ python-folder/tamarind.py
@@ -0,0 +1,78 @@
+# This script prints a while-loop with if-elif statement
+
+print('------ 16 Amazing Benefits of Tamarind ------')
+
+year = int(input("To see Tamarind's Benefits, Enter a number from 1 - 16: "))
+
+while year != 0:
+
+    if year == 1 :
+        print()
+        print('Helps with Weight Loss')
+        print()
+    elif year == 2:
+        print()
+        print('Helps with Stomach Ache & Constipation Relief')
+        print()
+    elif year == 3:
+        print()
+        print('Heals Liver injuries')
+        print()
+    elif year == 4:
+        print()
+        print('Boosts Heart Health')
+        print()
+    elif year == 5:
+        print()
+        print('Controls Diabetes and Hperglycemia')
+        print()
+    elif year == 6:
+        print()
+        print('Prevents Malaria and Microbial Diseases')
+        print()
+    elif year == 7:
+        print()
+        print('Helps with Exfoliation and Skin brightener')
+        print()
+    elif year == 8:
+        print()
+        print('Improves Circulation and Nerve Functions')
+        print()
+    elif year == 9:
+        print()
+        print('Improves Your Immune System')
+        print()
+    elif year == 10:
+        print()
+        print('Helps with Dental Health')
+        print()
+    elif year == 11:
+        print()
+        print('Can Promote Good Sexual Health')
+        print()
+    elif year == 12:
+        print()
+        print('May Boost Yout Taste Buds')
+        print()
+    elif year == 13:
+        print()
+        print('Helps the Brain Heal')
+        print()
+    elif year == 14:
+        print()
+        print('Promotes Good Sleeping Patterns')
+        print()
+    elif year == 15:
+        print()
+        print('Helps the Body Process Foods')
+        print()
+    elif year == 16:
+        print()
+        print('Boosts Your Vision')
+        print()
+    else:
+        print()
+        print('You entered an invalid entry')
+        print()
+    print("Enter '0' to quit at anytime to exit the script!")
+    year = int(input("To see Tamarind's Benefits, Enter a number from 1 - 16: "))
