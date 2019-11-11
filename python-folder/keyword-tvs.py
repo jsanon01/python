0e814062cdd0858e09468146b34179ffbf58121c
jean sanon <jeansanon180@gmail.com>
2019-11-10 19:20:38 -0500

    Initial commit

---
+++ python-folder/keyword-tvs.py
@@ -0,0 +1,8 @@
+# Script is about Prime Haitian TV stations
+# with keyword as function
+
+def keyword(channel_2, channel_8, channel_44, channel_22, channel_16):
+    print('Here are some prime  Haitian TV channels:\n ' + channel_2 + ' ' +  channel_8 + ' ' + channel_22 + ' '+ channel_16 +' ' + 'and ' +  channel_44)
+
+keyword(channel_2 = 'Tele-Haiti,', channel_8 = 'TNH,', channel_22 = 'Tele-Caraibes,', channel_44 = 'Tele Pluriel', channel_16 = 'Scoop,')
+
