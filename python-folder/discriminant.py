32b06cae8aed2f6289a5ca122535fe6307d72b56
jean sanon <jeansanon180@gmail.com>
2019-11-11 20:56:47 -0500

    Initial commit

---
+++ python-folder/discriminant.py
@@ -0,0 +1,15 @@
+# This script prints discriminant values
+
+
+print('This script prints discriminant values: ')
+
+a = int(input('Enter value a: '))
+
+b = int(input('Enter value b: '))
+
+c = int(input('Enter value c: '))
+
+formula = b * b - 4 * a * c
+
+print(f'The result for the discriminant is: {formula}')
+
