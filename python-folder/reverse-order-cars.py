cb1d62e57bc3e80532aaeb112fa57b8f19507d77
jean sanon <jeansanon180@gmail.com>
2019-11-16 18:13:48 -0500

    1st commit

---
+++ python-folder/reverse-order-cars.py
@@ -0,0 +1,11 @@
+# This script print list of cars in reverse order
+
+cars = ['kia', 'bmw', 'mercedes', 'ford', 'buick', 'nissan', 'toyota' ]
+print()
+print(f'Original list: ', cars)
+
+cars.reverse()
+
+print()
+print('Updated list:', cars)
+print()
