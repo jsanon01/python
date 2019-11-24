76ef0d2a529742c14c079f156ff57cb5186b6e65
jean sanon <jeansanon180@gmail.com>
2019-11-23 19:44:46 -0500

    Initial commit

---
+++ python-folder/format-method.py
@@ -0,0 +1,18 @@
+# This script prints variables using Format Method
+
+print('This script prints variables using Format Method')
+
+name = 'john doe'
+
+print("Hello {}".format(name).title())
+
+print('----------------------------------------------------')
+
+print('This script prints variables using Format Method')
+
+names = 'marie jeanne'
+
+print("How old is {}?".format(names).title())
+
+print("Well, {} is {} years old!".format(names, 29).title())
+
