5be768c3a9c4978be40c9ed264eeb855ff615bdc
jean sanon <jeansanon180@gmail.com>
2019-11-27 18:19:49 -0500

    Initial commit

---
+++ python-folder/car-info.py
@@ -0,0 +1,8 @@
+# This script prints thye Year, Make, Model, and Color of a vehicle
+
+year = int(input('Enter the vehicle year: '))
+make = input('Enter the vehicle make: ')
+model = input('Enter the vehicle model: ')
+color = input('Enter the vehicle color: ')
+
+print('The info you entered is: {} {} {} {}'.format(year, color, make, model).title())
