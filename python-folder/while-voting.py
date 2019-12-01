51a5cd630766714b3e4b2cd30808ff29bf952940
jean sanon <jeansanon180@gmail.com>
2019-12-01 14:22:49 -0500

    2nd commit

--- python-folder/while-voting.py
+++ python-folder/while-voting.py
@@ -1,11 +1,17 @@
 # This script is about a while-loop with Boolean (True)
 
-prompt = 'Enter a name: '
+
+print("Using this script, type 'q' for exit the loop!")
+
+prompt = 'Enter first + last names: '
 
 while True:
+
     name = input(prompt)
 
-    if name == 'quit':
-        break
+    if name == 'q':
+       
+         break
+ 
     else:
-        print(f'{name.title()} are you a registered voter?')
+            print(f'{name.title()} are you a registered voter?')
