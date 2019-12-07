5debb8420f1569510feeef378b8dfcad40639355
jean sanon <jeansanon180@gmail.com>
2019-12-07 18:24:41 -0500

    Initial commit

---
+++ python-folder/nested-forloop.py
@@ -0,0 +1,12 @@
+# This script prints a nested for loop
+
+
+print()
+
+print('This script prints a nested for loop')
+
+for i in range(2):
+    for j in range(3):
+        print('{}\t {}'.format(i, j))
+
+print()
