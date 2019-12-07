7831e3b59e380fe53e01a88fa015a8375775efcf
jean sanon <jeansanon180@gmail.com>
2019-12-06 19:53:46 -0500

    Initial commit

---
+++ python-folder/sports.py
@@ -0,0 +1,16 @@
+# This scripts prints a list of sports and players
+
+
+soccer = 'diego maradona'
+
+basket = 'michael jordan'
+
+tennis = 'laurent lamothe'
+
+golf = 'tiger woods'
+
+print()
+
+print('{} loves soccer!\n\n{} loves basket!\n\n{} loves tennis!\n\n{} loves Golf!'.format(soccer, basket, tennis, golf).title())
+
+print()
