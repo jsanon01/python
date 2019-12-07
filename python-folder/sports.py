63e45e794f0e093246a2f2b4ceee493301d267c5
jean sanon <jeansanon180@gmail.com>
2019-12-06 20:14:46 -0500

    2nd commit

--- python-folder/sports.py
+++ python-folder/sports.py
@@ -1,6 +1,10 @@
 # This scripts prints a list of sports and players
 
 
+print()
+
+print('This scripts prints a list of Sports and Players')
+
 soccer = 'diego maradona'
 
 basket = 'michael jordan'
@@ -12,5 +16,16 @@ golf = 'tiger woods'
 print()
 
 print('{} loves soccer!\n\n{} loves basket!\n\n{} loves tennis!\n\n{} loves Golf!'.format(soccer, basket, tennis, golf).title())
+
+print()
+
+print('---------------------------------------------')
+
+print()
+
+sports = ['soccer', 'basket', 'tennis', 'golf']
+
+for sport in sports:
+    print('Here are  the most popular sports: {}'.format(sport).title())
 
 print()
