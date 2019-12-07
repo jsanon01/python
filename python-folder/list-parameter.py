aac24fe9da547c5b6ad36a304103b92490408e7e
jean sanon <jeansanon180@gmail.com>
2019-12-07 18:40:50 -0500

    2nd commit

--- python-folder/list-parameter.py
+++ python-folder/list-parameter.py
@@ -1,4 +1,5 @@
 # This script passes a List as Parameter
+
 print('Script passes a List as Parameter')
 
 def my_function():
@@ -9,9 +10,10 @@ fruits = ['bananas', 'oranges', 'avocados', 'quenepes']
 
 my_function()
 
-print('-------------------------')
+print('----------------------------------------------------------')
 
 # This script passes a List as Parameter (demo from W3School.com)
+
 print('Script passes a List as Parameter')
 
 def function(food):
@@ -22,13 +24,18 @@ veggies = ['carrots', 'beans', 'sweet potatoes', 'brocoli']
 
 function(veggies)
 
-print('-----------------------')
+print('----------------------------------------------------------')
+
 # This script passes a List as Parameter (demo from W3School.com)
+
 print('Script calculates return values from calling function')
 
 def fonction(x):
+
     return 5 * x
 
 #print('fonction' * ( 3) # script will be displayed 3 times
+
 print(fonction(4)) # return value x calling fonction
+
 print(fonction(10)) # return value x calling fonction
