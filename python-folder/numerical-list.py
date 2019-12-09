1dcb0f5be775c02796a6854afa2ec004d4181948
jean sanon <jeansanon180@gmail.com>
2019-12-08 21:40:27 -0500

    Initial commit

---
+++ python-folder/numerical-list.py
@@ -0,0 +1,24 @@
+# This script prints numerical list
+
+
+
+print()
+
+print('Here is the following list:\nlist = [2, 4, 7, 11, 20, 29]')
+
+
+list = [2, 4, 7, 11, 20, 29]
+
+print()
+
+print('The minimum number in the list is: {}'.format(min(list)))
+
+print()
+
+print('The maximum number in the list is: {}'.format(max(list)))
+
+print()
+
+print('The sum of the numbers in the list is: {}'.format(sum(list)))
+
+print()
