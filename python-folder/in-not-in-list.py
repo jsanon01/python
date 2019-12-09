2d6db060c0c57f5b8c409a1bf3d86601937dcbfa
jean sanon <jeansanon180@gmail.com>
2019-12-08 21:39:45 -0500

    Initial commit

---
+++ python-folder/in-not-in-list.py
@@ -0,0 +1,15 @@
+# This script prints names found or not fopund in a list
+
+
+name = input('Enter a name: ')
+
+names = [ 'maguy', 'durce', 'marie', 'yolene', 'filipo']
+
+if 'yolene' in names:
+    print('found')
+else:
+    print('not found')
+
+
+#if 'dejean' not in names:
+ #   print('not found')
