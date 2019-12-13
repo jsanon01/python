63f79d5194915c129d1ba2f6ea0e8654a76b96a8
jean sanon <jeansanon180@gmail.com>
2019-12-12 21:09:11 -0500

    2nd commit

--- python-folder/functions.py
+++ python-folder/functions.py
@@ -1,7 +1,24 @@
+# This script prints out a function
+
+
+print()
+
+print('This script prints out a function with 3 calls: ')
+
+print()
 
 def ga(arrondissement = 'Grand-Anse'):
+
     print(arrondissement + ' fait partie de la Grand-Anse')
 
 ga('Abricots')
+
+print()
+
 ga('La Navase')
+
+print()
+
 ga('Pestel')
+
+print()
