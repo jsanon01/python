d66b26c129f547ff61343cd03dac4a75143f469e
jean sanon <jeansanon180@gmail.com>
2019-12-13 19:35:25 -0500

    2nd commit

--- python-folder/for-loop-variable-seq.py
+++ python-folder/for-loop-variable-seq.py
@@ -1,10 +1,19 @@
 # This script prints odd + even numbers using for-loop
 
-print('--- Printing Odd Sequence then Double-up ---')
+print()
 
-for i in [1,3,5,7,9]:
-    print(i * i)
+print('--- Printing Odd Number from 1-9 in Square Root ---')
 
-print('--- Printing Even Sequence then Double-up ---')
-for j in [2,4,6,8,10]:
-    print(j * j)
+for i in [1, 3, 5, 7, 9]:
+
+    print(f'Here is the Square Root of: ', i*i)
+
+print()
+
+print('--- Printing Even Number from 2-10 in Square Root ---')
+
+for j in [2, 4, 6, 8, 10]:
+
+    print(f'Here is the Square Root of: ', j*j)
+
+print()
