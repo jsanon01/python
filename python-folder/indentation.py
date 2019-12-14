8b6d836797d86afb28cf56e069504fef748b297c
jean sanon <jeansanon180@gmail.com>
2019-12-13 19:59:52 -0500

    2nd commit

--- python-folder/indentation.py
+++ python-folder/indentation.py
@@ -1,8 +1,26 @@
-# This script is about indentation
+# This script prints a function whose indentation is mandatory
+
+
+print('This script prints a function whose indentation is always mandatory.')
+
+print()
 
 indent = 'indentation'
 
 def my_function():
+
     print(f'Python loves {indent}!')
 
 my_function()
+
+print()
+
+commune = 'Marfranc'
+
+def ekip_solid():
+#    commune = 'Marfranc'
+    print(f"Fete de Fin d'annee a {commune} ")
+
+ekip_solid()
+
+print()
