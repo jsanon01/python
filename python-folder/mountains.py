882d7ef92076877a63927ffe74eaf0af1a885ee1
jean sanon <jeansanon180@gmail.com>
2019-12-15 17:58:59 -0500

    2nd commit

--- python-folder/mountains.py
+++ python-folder/mountains.py
@@ -1,11 +1,15 @@
 # This script prints contents of a list 
 #!/bin/env python3
 
+print()
+
 mountains = ['castaches','abricots', 'leon', 'bois-sec', 'bouda-mouille', 'duchiti']
 
 mountains.sort()
 
-print('Here are the following mountains in Grand-Anse Department:')
+print('This script prints out the following mountains in Grand-Anse Department: ')
 
 for mountain in mountains:
-    print(mountain.title())
+    print(f'Here are the following mountains in Grand-Anse Department: {mountain.title()}')
+
+print()
