9c2e9a7a5447dad95ab8d18fd3cbef6f5695cd55
jean sanon <jeansanon180@gmail.com>
2019-12-16 20:47:27 -0500

    Initial commit

---
+++ python-folder/pizzas-toppings2.py
@@ -0,0 +1,18 @@
+# This script prints out 2 functions along with pizzas' ingredients
+
+print()
+
+toppings1 = 'cheese, onions, pepperoni, mushrooms'
+
+toppings2 = 'flour, sliced-salami, green pepper, and olive...'
+
+print('Here are the following ingredients to make pizzas: ')
+
+def pizzas(toppings1, toppings2):
+
+    print('{} along with {} '.format(toppings1, toppings2).title())
+
+
+pizzas(toppings1, toppings2)
+
+print()
