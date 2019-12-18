bfa83b475bfc88b796905c3234ed3600ede6662e
jean sanon <jeansanon180@gmail.com>
2019-12-17 21:03:46 -0500

    Initial commit

---
+++ python-folder/function-stages.py
@@ -0,0 +1,17 @@
+# This script prints out function stages
+
+print()
+
+print("This script prints out 2 steps of a function lifecycle:\n'Function Definition and Function Call'")
+
+def contactInfo():
+    print('Name: Joel, Osteen')
+    print('Age: 50 years old')
+    print('Address: 145 Maple St')
+    print('City: Dallas, TX')
+    print('Zip Code: 14798')
+    print('Tel: 800-987-3210')
+
+contactInfo()
+
+print()
