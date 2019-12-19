4833f90441b88dc30cfcd27d10ff4f9066f27e37
jean sanon <jeansanon180@gmail.com>
2019-12-18 19:50:56 -0500

    Initial commit

---
+++ python-folder/function-parameters.py
@@ -0,0 +1,26 @@
+# This script prints parameters inside functions
+
+print()
+
+print('This script prints out parameters that are found inside functions')
+
+def Grand_Anse(arrondissement, communes, section_communales):
+
+    print("Voici les communes du departement de la Grand-Anse:\n{}, {} et {}".format(arrondissement, communes,section_communales).title())
+
+print()
+
+
+Grand_Anse('jeremie', 'anse-dainault', 'corail')
+
+print()
+
+print('------------------------------------------------------')
+
+def Grand_Anse(quartier, quartiers, katye, katyie):
+
+    print("Voici les quartiers de L'Arrondissement de Jeremie:\n{}, {}, {} et {}".format(quartier, quartiers, katyie, katye).title())
+
+Grand_Anse('bordes', 'marfranc', 'leon', 'beaumont')
+
+print()
