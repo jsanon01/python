3e1df9204ca946e8f153e1634a374b24fea6fae1
jean sanon <jeansanon180@gmail.com>
2019-12-20 09:39:41 -0500

    Initial commit

---
+++ python-folder/inner-functions.py
@@ -0,0 +1,37 @@
+# This script prints inner and outer functions
+
+print()
+
+print('This script prints inner and outer functions')
+
+def function1():
+
+    print('Hello from outer function!')
+
+    def function2():
+
+        print('Hello from inner function!')
+
+    function2()
+
+function1()
+
+print()
+
+print('--------------------------------------------------------')
+
+print('This script prints inner and outer functions')
+
+def Grand_Anse():
+
+    print('Grand Anse is the department!')
+
+    def Jeremie():
+
+        print('Jeremie is the arrondissement!')
+
+    Jeremie()
+
+Grand_Anse()
+
+print()
