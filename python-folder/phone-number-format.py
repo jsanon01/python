0bae3a64a586bf60ad6606d3412033683ed928a0
jean sanon <jeansanon180@gmail.com>
2019-12-24 22:55:17 -0500

    Initial commit

---
+++ python-folder/phone-number-format.py
@@ -0,0 +1,12 @@
+# This script formats phone number
+
+print()
+
+print('Phone Number prints in the following format:\t(XXX)-XXX-XXXX')
+
+num = input('Enter number: ')
+
+#print("("+num[:3]+")"+num[3:6]+"-"+num[6:])
+print("\nYou entered: ("+num[:3]+")"+num[3:6]+"-"+num[6:])
+
+print()
