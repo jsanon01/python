382b621da7f6bfc980b4f208d8d90373072d2b98
jean sanon <jeansanon180@gmail.com>
2019-12-25 20:31:13 -0500

    Initial commit

---
+++ python-folder/function-and-lists.py
@@ -0,0 +1,21 @@
+#
+
+num1 = [2,4,6,8,10]
+
+num2 = [1,3,5,7,9]
+
+def squares(numbers):
+
+    for num in numbers:
+
+        print(num ** 2)
+
+print('Square root of the following list: [2,4,6,8,10]')
+
+squares(num1)
+
+print('-------------------------------------------------------')
+
+print('Square root of the following list: [1,3,5,7,9]')
+
+squares(num2)
