49fe5791c0942762ac0a96f43fc8de326566e1df
jean sanon <jeansanon180@gmail.com>
2019-12-28 14:35:56 -0500

    Initial commit

---
+++ python-folder/function-ternary-operator.py
@@ -0,0 +1,15 @@
+# This script prints out a function along with 'Ternary Operator'
+
+print("\nThis script prints out a function along with 'Ternary Operator'")
+
+print()
+
+def searchList(aList, el):
+
+    return True if el in aList else False
+
+result = searchList(['one', 2, 'three'], 2)
+
+print('The result is: {}'.format(result))
+
+print()
