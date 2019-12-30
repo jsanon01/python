4c0c037f054a0a8028b55037d77bd368e6985bb0
jean sanon <jeansanon180@gmail.com>
2019-12-29 19:18:57 -0500

    Initial commit

---
+++ python-folder/function-object.py
@@ -0,0 +1,26 @@
+# This script prints out a function
+
+print()
+
+print('Everything in Python is considered as object.\nSo these 2 functions are also objects: ')
+
+def function1():
+
+    function1.var = 'josma, joseph, auguste'
+
+    print(function1.var.title())
+
+
+def function2():
+
+    print(function1.var.upper())
+
+print('\nThe Natives of Moron are: ')
+
+function1()
+
+print('-------------------------------')
+
+function2()
+
+print()
