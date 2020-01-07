b7d0104b2f3dbfdc900eebc19675db9d63881718
jean sanon <jeansanon180@gmail.com>
2020-01-06 21:43:21 -0500

    Initial commit

---
+++ python-folder/grandanse-geo.py
@@ -0,0 +1,46 @@
+"""
+I want to print a function 'Dept of Grand-Anse'
+I want to print a function 'Capital of Grand-Anse'
+I want to print a function 'Major cities of Grand-Anse'
+I want to print a function 'Major towns of Grand-Anse'
+
+I want a main function to call: dept(), capital(), cities(), towns()
+
+I want 'Dept of Grand-Anse' to be called when x = 1
+I want 'Dept of Grand-Anse' to be called when x = 2
+I want 'Dept of Grand-Anse' to be called when x = 3
+I want 'Dept of Grand-Anse' to be called when x = 4
+
+
+"""
+print()
+
+def dept():
+    print('Grand-Anse Dept is located in Southern west side of Haiti.')
+
+#dept()
+
+def capital():
+    print("\nThe capital of Grand-Anse Dept is Jeremie aka 'The city of Poetry...'")
+
+#capital()    
+
+def cities():
+    print("\nThe major cities of Grand-Anse Dept are:\nJeremie\t\tAnse-Dainault\t\tDame-Marie\tLes Irois\nAbricots\tMarfranc\t\tMoron\t\tChambellan\nCorail\t\tPestel\t\t\tBeaumont...")
+
+#cities()
+
+def towns():
+    print("\nThe towns are:\nJeremie\t\tLeon\tFond-Cochon...")
+
+#towns()
+
+def main():
+    dept()
+    capital()
+    cities()
+    towns()
+
+main()
+
+print()
