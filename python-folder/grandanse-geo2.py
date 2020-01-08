a0c51e4cc7a447c7e2299eaa8e4a40b6535c85f4
jean sanon <jeansanon180@gmail.com>
2020-01-07 20:22:12 -0500

    2nd commit

--- python-folder/grandanse-geo2.py
+++ python-folder/grandanse-geo2.py
@@ -3,14 +3,14 @@ I want to print a function 'Dept of Grand-Anse'
 I want to print a function 'Capital of Grand-Anse'
 I want to print a function 'Major cities of Grand-Anse'
 I want to print a function 'Major towns of Grand-Anse'
+I want to print a function 'Famous people in Grand-Anse'
 
-I want a main function to call: dept(), capital(), cities(), towns()
-
-I want 'Dept of Grand-Anse' to be called when x = 1
-I want 'Dept of Grand-Anse' to be called when x = 2
-I want 'Dept of Grand-Anse' to be called when x = 3
-I want 'Dept of Grand-Anse' to be called when x = 4
-
+I want a main function to call: dept(), capital(), cities(), towns(), people()
+I want 'Dept of Grand-Anse' to be called when x = 1 in the main function
+I want 'Capital of Grand-Anse' to be called when x = 2 in the main function
+I want 'Major Cities of Grand-Anse' to be called when x = 3 in the main function
+I want 'Major towns of Grand-Anse' to be called when x = 4 in the main function
+I want 'Famous people of Grand-Anse' to be called when x = 5 in the main function
 
 """
 print()
@@ -33,13 +33,31 @@ def cities():
 def towns():
     print("\nThe towns are:\nJeremie\t\tLeon\tFond-Cochon...")
 
-#towns()
+def people():
+    print("\nThe Famous people are:\netzer vilaire\t\tjean briere\t\tedmond laforest\nlesly center\t\teddy rene\t\twifrid simeon\ngary rouzier\t\tpatrick moussignac\tLes Cedras...")
+
+print('This script prints Grand-Anse Geo areas & Famous People through a main loop.\n[1] Dept\t\t[2] Capital\t[3] Cities\t[4] Towns\n[5] People\t\t[0] Quit: ')
 
 def main():
-    dept()
-    capital()
-    cities()
-    towns()
+    x = int(input('\nEnter a number from 0 - 5: '))
+    
+    while x:
+
+        if x == 1:
+            dept()
+        elif x == 2:
+            capital()
+        elif x == 3:
+            cities()
+        elif x == 4:
+            towns()
+        elif x == 5:
+            people()
+        else:
+            print('Invalid Number!')
+        x = int(input('Enter a number from 0 - 5: '))
+
+    print('\nYou have exited the script.')
 
 main()
 
