3eee62b929cfb9c431c61a4acc2e60b19829dd3d
jean sanon <jeansanon180@gmail.com>
2020-01-09 21:42:22 -0500

    Initial commit

---
+++ python-folder/serverless.py
@@ -0,0 +1,13 @@
+
+
+def compute():
+    print('Lambda lets you run code without provisioning servers.\n- Lamda Edges manages CloudFront.\nFargate is a built-purpose containers.')
+
+def storage():
+    print('- S3 provides secure web interface from any web around the world.\- EFS provides simple, and elastic file storage.')
+
+def main():
+    compute()
+    storage()
+
+main()
