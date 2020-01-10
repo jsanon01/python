bb6339fdc63a9e42e69bcc1ec47b8ac8ec238087
jean sanon <jeansanon180@gmail.com>
2020-01-09 21:51:45 -0500

    2nd commit

--- python-folder/serverless.py
+++ python-folder/serverless.py
@@ -1,4 +1,13 @@
+"""
+I want a function named 'compute'
+I want a function named 'storage'
+I want a function named 'data_stores'
+I want a function named 'api_proxy'
+I want a function named 'api_integration'
+I want a function named 'analytics'
+I want a function named 'dev_tool'
 
+"""
 
 def compute():
     print('Lambda lets you run code without provisioning servers.\n- Lamda Edges manages CloudFront.\nFargate is a built-purpose containers.')
@@ -6,8 +15,40 @@ def compute():
 def storage():
     print('- S3 provides secure web interface from any web around the world.\- EFS provides simple, and elastic file storage.')
 
+def data_stores():
+    print('Dynamo DB is no sql.')
+
+def api_proxy():
+    print('API Gateway...')
+    
+def api_integration():
+        print('API Integration...')
+
+def analytics():
+        print('Analytics.....')
+
+def dev_tool():
+        print('Dev tool....')
+
+
+
+
+
+
 def main():
     compute()
+    print()
     storage()
+    print()
+    data_stores()
+    print()
+    api_proxy()
+    print()
+    api_integration()
+    print()
+    analytics()
+    print()
+    dev_tool()
+    print()
 
 main()
