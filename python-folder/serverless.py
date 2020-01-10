0303015c45ef13912e9c2e5bc510f33c94a4c8cc
jean sanon <jeansanon180@gmail.com>
2020-01-10 10:16:09 -0500

    4th commit

--- python-folder/serverless.py
+++ python-folder/serverless.py
@@ -1,4 +1,5 @@
 """
+I want to import website for Reference
 I want a function named 'compute'
 I want a function named 'storage'
 I want a function named 'data_stores'
@@ -9,7 +10,10 @@ I want a function named 'dev_tool'
 
 I want to print out a menu to display 'main loop function'
 I want a while statement inside the  main function
+
 """
+import webbrowser
+
 
 def compute():
     print('Compute:\n- Lambda lets you run code without provisioning servers.\n- Lamda Edges manages CloudFront.\n- Fargate is a built-purpose containers.')
@@ -39,21 +43,41 @@ print("\nThis script prints out a Menu to display 'Main Loop function.'")
 
 
 def main():
-    print("\nHere are the following AWS 'serverless' services:\n[0] Quit\t[1] Compute\t\t[2] Storage\t[3] Data Stores\n[4] API Proxy\t[5] API Integration\t[6] Analytics\t[7] Dev Tools\n[8] Reference ")
-    print()
-    compute()
-    print()
-    storage()
-    print()
-    data_stores()
-    print()
-    api_proxy()
-    print()
-    api_integration()
-    print()
-    analytics()
-    print()
-    dev_tool()
-    print()
+
+    print("\nHere are the following AWS 'serverless' services:\n[0] Quit\t[1] Compute\t\t[2] Storage\t[3] Data Stores\n[4] API Proxy\t[5] API Integration\t[6] Analytics\t[7] Dev Tools\n[8] Website ")
+
+    aws = int(input('Enter a number from 0 - 8: '))
+
+    while aws:
+    
+        if aws == 1:
+            compute()
+
+        elif aws == 2:
+            storage()
+
+        elif aws == 3:
+            data_stores()
+
+        elif aws == 4:
+            api_proxy()
+
+        elif aws == 5:
+            api_integration()
+
+        elif aws == 6:
+            analytics()
+
+        elif aws == 7:
+            dev_tool()
+
+        elif aws == 8:
+            webbrowser.open('https://aws.amazon.com/serverless/')
+
+        else:
+           print('Invalid number.')
+        aws = int(input('Enter a number from 0 - 8: '))
+
+    print('Exiting the script...!')
 
 main()
