81a8f324aa37dd84dde7dd59d76c3127f463c625
jean sanon <jeansanon180@gmail.com>
2020-01-13 22:34:25 -0500

    Initial commit

---
+++ python-folder/list-dictionary.py
@@ -0,0 +1,26 @@
+# This script prints out a List including a dictionary
+
+print()
+
+data = ['jean','claude','duvalier', {'57th':'former president haitien'}]
+
+
+print('This script prints out a List with indexes including a dictionary.')
+
+print('\nHere is the original list: ')
+
+print(data)
+
+print('\nHere are the following separated indexes in the list: ')
+
+print(data[0].title())
+
+print(data[1].title())
+
+print(data[2].title())
+
+print('\nHere is the dictionary in the list: ')
+
+print(data[3]['57th'].title())
+
+print()
