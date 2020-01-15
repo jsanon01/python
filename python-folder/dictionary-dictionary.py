b6474761e56e948cf1025b79e9382fa81c635e7a
jean sanon <jeansanon180@gmail.com>
2020-01-14 20:27:57 -0500

    2nd commit

--- python-folder/dictionary-dictionary.py
+++ python-folder/dictionary-dictionary.py
@@ -1,5 +1,5 @@
 """
-I want to print a dictionary with keys and values
+I want to print a main dictionary and sub-dictionary with keys and values
 I want to print a dictionary inside main dictionary with keys and values
 I want to print indexes in a dictionary
 
@@ -7,9 +7,21 @@ I want to print indexes in a dictionary
 
 data = {
 	'teams': 'Red Sox, Bruins, Patriots',
-#	'wins': {'2018': 102, '2019': 36}
+	'wins': {'year': 2018, 'score': '36-12'}
 }
 
-print('\nI want to print a dictionary with keys and values')
+print("\nI want to print 'main & sub dictionaries with keys and values'")
 
 print('{}'.format(data))
+
+print('\nI want to print a dictionary inside main dictionary with keys and values')
+
+print("The sub-dictionary here is 'wins':\n {}".format(data['wins']))
+
+print("\nI want to print indexes inside sub-dictionary 'wins'")
+
+print('Year: {}'.format(data['wins']['year']))
+
+print('Score: {}'.format(data['wins']['score']))
+
+print()
