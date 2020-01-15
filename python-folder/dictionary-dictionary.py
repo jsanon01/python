8f8dfd1f06598f94b15a1709a62276c5c1985fa7
jean sanon <jeansanon180@gmail.com>
2020-01-14 20:02:42 -0500

    Initial commit

---
+++ python-folder/dictionary-dictionary.py
@@ -0,0 +1,15 @@
+"""
+I want to print a dictionary with keys and values
+I want to print a dictionary inside main dictionary with keys and values
+I want to print indexes in a dictionary
+
+"""
+
+data = {
+	'teams': 'Red Sox, Bruins, Patriots',
+#	'wins': {'2018': 102, '2019': 36}
+}
+
+print('\nI want to print a dictionary with keys and values')
+
+print('{}'.format(data))
