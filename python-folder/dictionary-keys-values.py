536413035044dc3cb3aae795331698f27797b749
jean sanon <jeansanon180@gmail.com>
2020-01-15 21:24:50 -0500

    2nd commit

--- python-folder/dictionary-keys-values.py
+++ python-folder/dictionary-keys-values.py
@@ -3,6 +3,8 @@ I want to initiate an empty dictionary
 I want to initiate/label 4 keys and values inside that dictionary
 I want to use for statement to print out dictionary's keys and values
 
+I want to print out the dictionary
+I want to print out the dictionary using for-loop
 
 """
 
@@ -17,7 +19,13 @@ dictionary['black'] = ['kirk franklin']
 
 dictionary['african'] = ['sinach osinachi']
 
+print('\n--------- This script prints out regular dictionary ----------')
+
+print('{}'.format(dictionary).title())
+
+print('\n--------- This script uses for-loop to print out same dictionary ----------')
 
 for k, v in dictionary.items():
     print('Performing Gospel artists: - {} --> {}'.format(k,v).title())
 
+print()
