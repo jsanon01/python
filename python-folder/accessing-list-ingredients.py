fb09ff60d60788ee956ab43bb97f35fb8c15c0c5
jean sanon <jeansanon180@gmail.com>
2020-01-18 21:53:55 -0500

    2nd commit

--- python-folder/accessing-list-ingredients.py
+++ python-folder/accessing-list-ingredients.py
@@ -1,3 +1,14 @@
+"""
+I want to assign a dictionary with key and values using a list
+I want to print original dictionary
+I want to access all values in the dictionary's list
+I want to access 1st value in the dictionary's list
+I want to access last value in the dictionary's list
+
+I want to print out the same dictionary using for-loop 
+
+"""
+
 
 pizza = {
 	'ingredients': ['cheese', 'onion', 'peperoni', 'sausage']
