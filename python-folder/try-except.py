7bb32b84cfaf73e8f8954d646e712e5c62a9706d
jean sanon <jeansanon180@gmail.com>
2020-01-20 19:21:39 -0500

    2nd commit

--- python-folder/try-except.py
+++ python-folder/try-except.py
@@ -1,11 +1,11 @@
 """
-I want to print a dictionary with keys and values
+I want to print a dictionary with given keys and values
 I want to delete key-value pairs using try-except function
 
 """
 
-#print("\nThis script prints out 'remaining' key-value pairs in a dictionary.")
-#print("\nOriginal list:\n{'year': 2018, 'Year': 2019 , 'years': 2020 }")
+print("\nThis script prints out 'remaining' key-value pairs in a dictionary.")
+print("\nOriginal list:\n{'year': 2018, 'Year': 2019 , 'years': 2020 }")
 
 dictionary = {
 		'year': 2018 ,
@@ -16,6 +16,7 @@ dictionary = {
 try:
     del dictionary['year']
     print('\nUpdated List:\n{}'.format(dictionary))
+
 except:
     print('Key does not exist.')
 
