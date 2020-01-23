7405441405e94b1ad59ed55c05f05a1b22b2c618
jean sanon <jeansanon180@gmail.com>
2020-01-22 21:18:52 -0500

    2nd commit

--- python-folder/traffic-light.py
+++ python-folder/traffic-light.py
@@ -1,8 +1,16 @@
+"""
+I want to call PIL from library
+I want to open an image named 'logo.png'
+I want to use a while statement for the 3 colors: red, yellow, and green
+
+"""
 from PIL import Image, ImageFilter
 
 original = ''
 
 original = Image.open('logo.png')
+
+print("\nThis script prints out the following:\n[red]  [yellow]  [green]  ['open' an image]  ['q' for Quit]")
 
 while True:
     
@@ -12,16 +20,16 @@ while True:
         break
 
     elif color == 'red':
-        print('COMPLETE STOP!')
+        print('Red leads to a complete STOP!')
     elif color == 'yellow':
         print('WARNING... Be Ready to Stop!')
     elif color == 'green':
-        print('GO AHEAD!')
+        print('Green means Go...GO AHEAD!')
     elif color == 'open':
         original.show()
     else:
         print('Invalid entry...!')
 
-print('Exited the loop')
+print('You have exited the loop...')
 
 print()
