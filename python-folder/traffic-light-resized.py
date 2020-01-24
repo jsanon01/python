0496e7333c75c845b9de4e66ebdfa9db3bc3762b
jean sanon <jeansanon180@gmail.com>
2020-01-23 21:04:01 -0500

    2nd commit

--- python-folder/traffic-light-resized.py
+++ python-folder/traffic-light-resized.py
@@ -1,3 +1,12 @@
+"""
+I want to import Python Library
+I want to initalize 'img' to open image file
+I want to calculate percentage and size
+I want to save image in another file
+
+Reference: https://opensource.com/life/15/2/resize-images-python
+"""
+
 import PIL
 from PIL import Image
 
@@ -12,6 +21,5 @@ hsize = int((float(img.size[1]) * float(wpercent)))
 
 img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
 
-#img.save('resized_image.jpg')
 img.save('traffic-lights2.jpg')
 
