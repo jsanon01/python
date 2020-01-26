130b55e6731dcb62c1e90796eeba19c0537bf421
jean sanon <jeansanon180@gmail.com>
2020-01-25 22:26:28 -0500

    2nd commit

--- python-folder/csv-user-input.py
+++ python-folder/csv-user-input.py
@@ -1,5 +1,6 @@
 """
-I want to ask the user for its name
+I want to ask the user for its first name
+I want to ask the user for its last name
 I want to ask the user for its phone number
 I want the program to save name and phone number in a given file
 
@@ -8,22 +9,31 @@ import csv
 
 print("\nThis script prints out name and phone in a text file.")
 
-name = input('Enter name: ')
+fname = input('Enter first name: ')
+
+lname = input('Enter last name: ')
 
 phone = input('Enter a number: ')
 
 
 f = open('jodel.txt', 'w')
 
-f.write("Name\t\t\tPhone\n")
-f.write('{}\t\t'.format(name).title())
-#f.write(' ')
+print()
+
+f.write("First Name\tLast Name\tPhone\n")
+
+f.write('---------------------------------------------')
+
+f.write('\n{}\t\t'.format(fname).title())
+
+f.write('{}\t\t'.format(lname).title())
+
 f.write('{}\t\t\t'.format(phone).title())
+
 f.write('\t\t\t')
 
 
 f.close()
-
 
 f = open('jodel.txt', 'r')
 
