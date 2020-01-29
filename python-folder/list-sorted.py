9d8acb09a146b45cb2dd1f3f729b83fc522f5932
jean sanon <jeansanon180@gmail.com>
2020-01-28 20:55:31 -0500

    Initial commit

---
+++ python-folder/list-sorted.py
@@ -0,0 +1,15 @@
+"""
+I want to print a sorted list
+
+"""
+
+list = [9,6,3,1,4,8,0]
+
+print('This script prints out a list in ascending order: ')
+print("Original lsitnlist = [9,6,3,1,4,8,0]")
+ 
+print(sorted(list))
+print()
+list.sort()
+print(list)
+
