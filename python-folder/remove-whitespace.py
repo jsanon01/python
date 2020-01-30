6c9d9858f7c0109803fbff5b2bc682a36952c4b5
jean sanon <jeansanon180@gmail.com>
2020-01-29 21:48:52 -0500

    2nd commit

--- python-folder/remove-whitespace.py
+++ python-folder/remove-whitespace.py
@@ -1,20 +1,22 @@
 """
 I want to remove spaces from string using strip
+- 1) original string
+- 2) new string
 
 """
 
+print('\nThis script removes white spaces on string:')
 
-print('\nThis script removes white space on string')
+print("\nOriginal string is:\n' jean level   ' and '  joe namphy   '")
+
+print()
 
 level = ' jean level   '.strip()
 
 namphy = "  joe namphy   ".strip()
 
-print(f'{level.title()} and {namphy.title()} are friends.')
+print(f'New string is:\n{level} and {namphy}')
 
-
-
-#print(' {} and {} are friends'.format(fname, lname).strip())
-
+# print(f'New string is:\n{level.title()} and {namphy.title()}') # code is great
 
 print()
