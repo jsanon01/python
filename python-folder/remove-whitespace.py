5ab4600137fac5775ed2cecc95202c0c56b3681c
jean sanon <jeansanon180@gmail.com>
2020-01-29 21:24:49 -0500

    Initial commit

---
+++ python-folder/remove-whitespace.py
@@ -0,0 +1,20 @@
+"""
+I want to remove spaces from string using strip
+
+"""
+
+
+print('\nThis script removes white space on string')
+
+level = ' jean level   '.strip()
+
+namphy = "  joe namphy   ".strip()
+
+print(f'{level.title()} and {namphy.title()} are friends.')
+
+
+
+#print(' {} and {} are friends'.format(fname, lname).strip())
+
+
+print()
