8a7dedbe15880490c817a0129324c263ba0a7179
jean sanon <jeansanon180@gmail.com>
2020-02-01 20:42:31 -0500

    3rd commit

--- python-folder/class-attributes.py
+++ python-folder/class-attributes.py
@@ -9,14 +9,28 @@ print("\nThis script prints out 2 attributes of 'class Car': color and sound.")
 
 class Car():
 
-    sound = 'beep, beep' # sound attribute
+    door = '4 doors' # sound attribute
 
     color = 'red' # color attribite
 
 datsun = Car()
 
-print('\nThe color of this Datsun is: {}'.format(datsun.color)) # accessing object attribute with dot (.) syntax
+print('\nThis Datsun has the following characteristics: {} color and {}'.format(datsun.color, datsun.door)) # accessing object attribute with dot (.) syntax COLLECTIVELY
 
-print('\nThe sound of this Datsun is: {}'.format(datsun.sound)) # accessing object attribute wit dot (.) syntax
+#print('\nThe color of this Datsun is: {}'.format(datsun.color)) # accessing object attribute with dot (.) syntax INDIVIDUALLY
+
+#print('\nThe sound of this Datsun is: {}'.format(datsun.sound)) # accessing object attribute wit dot (.) syntax INDIVIDUALLY
 
 print()
+
+"""
+class Humans():
+    bio = 'bios = life and logy = study'
+    neuro = 'neuro  = nerves and logy = study'
+    ophtha = 'ophtha  = eyes and logy = study'
+
+john = Humans()
+
+print(john.bio)
+
+"""
