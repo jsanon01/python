7a47e28a6a11e25dda9a4c1ce819eaf062d0fa1e
jean sanon <jeansanon180@gmail.com>
2020-02-02 12:55:14 -0500

    Initial commit

---
+++ python-folder/class-instances.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+
+class Car():
+    pass
+
+bmw = Car() # creating instance & storing it in bmw's variable
+
+ford = Car()
+
+#print('\nLocation in memory: {}'.format((bmw))
+
+print('\nNumerical representation of memory location:')
+print(hash(bmw))
+print()
+
+print('\nNumerical representation of memory location:')
+print(hash(ford))
+print()
