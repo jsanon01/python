60bcb60c925fd967e5651b55a07e0b0f5edcd8cd
jean sanon <jeansanon180@gmail.com>
2020-02-02 13:15:37 -0500

    2nd commit

--- python-folder/class-instances.py
+++ python-folder/class-instances.py
@@ -1,6 +1,22 @@
 """
+I want to define a class named Vehicle
+I want to print out the memory location of that class
+
+I want to define a class named Car
+I want to create 2 objects from the class Car
+I want to print them out
 
 """
+
+class Vehicle():
+    pass
+
+kia = Vehicle()
+
+print('\nMemory location of the class Vehicle():\n {}'.format(kia))
+print()
+
+print('--------------------------------------------------------------------')
 
 class Car():
     pass
@@ -9,12 +25,9 @@ bmw = Car() # creating instance & storing it in bmw's variable
 
 ford = Car()
 
-#print('\nLocation in memory: {}'.format((bmw))
-
-print('\nNumerical representation of memory location:')
+print("\nNumerical representation of memory location of variable bmw':\n'bmw = Car' ")
 print(hash(bmw))
-print()
 
-print('\nNumerical representation of memory location:')
+print("\nNumerical representation of memory location of variable ford':\n'ford = Car' ")
 print(hash(ford))
 print()
