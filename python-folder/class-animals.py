3d4a0de556108fedae4f372540d1a94bbd1610ab
jean sanon <jeansanon180@gmail.com>
2020-02-03 21:10:09 -0500

    Initial commit

---
+++ python-folder/class-animals.py
@@ -0,0 +1,20 @@
+"""
+I want to create a class named 'Animals'
+I want to create 2 instances from that class
+I want to print them out
+
+"""
+class Animals():
+    male = 'lion is the king of animals'
+    female = 'tiger is from bobcat family'
+
+lion = Animals()
+
+tiger = Animals()
+
+#lion.Animals()
+
+print(lion.male)
+
+print(tiger.female)
+
