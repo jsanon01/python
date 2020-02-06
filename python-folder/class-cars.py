5fcfd32db89c408a72e285918e5891b9bed1282a
jean sanon <jeansanon180@gmail.com>
2020-02-05 21:31:07 -0500

    2nd commit

--- python-folder/class-cars.py
+++ python-folder/class-cars.py
@@ -3,7 +3,7 @@ I want to define a class Cars
 I want to define different values for multiple instances
 
 """
-print('\nThis script prints...')
+print('\nThis script prints out attributes from class Cars()...')
 
 class Cars():
     def __init__(self, year, brand, color):
@@ -13,16 +13,20 @@ class Cars():
 
 bmw = Cars(2020, 'bmw', 'red') # creating 2020 BMW red car object
 
-nissan = Cars(2017, 'Nissan', 'blue') # creating 2017 Nissan blue car object
+nissan = Cars(2017, 'nissan', 'blue') # creating 2017 Nissan blue car object
 
-audi = Cars(2019, 'AUDI', 'white') # creating 2019 AUDI white car object
+audi = Cars(2019, 'audi', 'white') # creating 2019 AUDI white car object
 
-#Cars.sort()
+print()
 
 print(bmw.year, bmw.brand.upper(),bmw.color.title())
 
-print(audi.year, audi.brand, audi.color)
+print()
 
-print(nissan.year, nissan.brand, nissan.color)
+print(audi.year, audi.brand.upper(), audi.color.title())
+
+print()
+
+print(nissan.year, nissan.brand.upper(), nissan.color.title())
 
 print()
