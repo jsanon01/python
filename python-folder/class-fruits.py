5e9ec52df33c5bf4b68b1d6504e51e8811772da1
jean sanon <jeansanon180@gmail.com>
2020-02-06 09:49:53 -0500

    2nd commit

--- python-folder/class-fruits.py
+++ python-folder/class-fruits.py
@@ -2,13 +2,13 @@
 I want to define a class named Fruits()
 I want to define some attributes inside the class named Fruits()
 I want to print out these attributes related to class named Fruits()
-
 """
+
 class Fruits():
     def __init__(self, apples, bananas, mangos):
-        self.apples = apples
-        self.bananas = bananas
-        self.mangos = mangos
+        self.apples = apples # setting attribute to the value passed
+        self.bananas = bananas # setting attribute to the value passed
+        self.mangos = mangos # setting attribute to the value passed
 
 zabokas = Fruits('imported', 'local produce', 'local produce')
 
@@ -16,4 +16,6 @@ apples = 'few people eat apples'
 bananas = 'popular and year-around'
 mangos = 'popular and seasonable'
 
-print('Here are some favorite Haitian fruits:\n- Apples are {}\n- Bananas are {}\n- Mangos are {}'.format(zabokas.apples, zabokas.bananas, zabokas.mangos))
+print('\nHere are some favorite Haitian fruits:\n- Apples are {}\n- Bananas are {}\n- Mangos are {}'.format(zabokas.apples, zabokas.bananas, zabokas.mangos))
+
+print()
