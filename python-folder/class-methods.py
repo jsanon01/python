44b24d8a1cec8605ba5ab995bb0add61cd9fe6d0
jean sanon <jeansanon180@gmail.com>
2020-02-11 16:37:36 -0500

    Initial commit

---
+++ python-folder/class-methods.py
@@ -0,0 +1,21 @@
+"""
+I want to define a class named Dog
+I want to define a class named Dog
+I want to define a class named Dog
+
+"""
+class Dog:
+    def makingsound(self):
+        print('bark')
+
+    def makesound(self):
+        print('meow')
+
+sam = Dog()
+print()
+sam.makingsound()
+
+kitty = Dog()
+print()
+kitty.makesound()
+print()
