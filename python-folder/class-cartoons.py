c6f62621da70667125e0ff597ae7645145afd4fb
jean sanon <jeansanon180@gmail.com>
2020-02-13 16:46:17 -0500

    2nd commit

--- python-folder/class-cartoons.py
+++ python-folder/class-cartoons.py
@@ -4,6 +4,7 @@ I want to define a class methods inside class Cartoons
 I want to call these class methods
 
 """
+
 class Cartoons:
     def pbs(self): 
         print(f"'Daniel the tiger', 'Superwhy','nature cat'".title())
@@ -11,6 +12,11 @@ class Cartoons:
     def qubo(self):
         print(f'the cat with the hat knows a lot about that'.title())
 
+    def daystar(self):
+        print(f'DayStar is an evanlegical tv channel broadcasting worlwide'.title())
+
+
+print("\nThis script prints out class methods with 'self' parameters: ")
 daniel = Cartoons()
 print()
 daniel.pbs()
@@ -18,5 +24,10 @@ daniel.pbs()
 nadia = Cartoons()
 print()
 nadia.qubo()
+print()
+
+tv = Cartoons()
+print()
+tv.daystar()
 print()
 
