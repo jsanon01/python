3720fd89ffd24d6e772d376b1593f3bf5e2d63a4
jean sanon <jeansanon180@gmail.com>
2020-02-14 23:38:58 -0500

    Initial commit

---
+++ python-folder/class-jkb.py
@@ -0,0 +1,11 @@
+"""
+I want to define a class named JKB()
+I want to define 2 class methods inside JKB
+I want to print out these class methods
+
+"""
+class JKB:
+    def moron(self):
+        print('Moronais')
+    def leon(self):
+        print('leonais')
