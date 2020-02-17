0dc7b0658adbc7f8ba30ef45060f17c192ee3f7b
jean sanon <jeansanon180@gmail.com>
2020-02-17 16:54:48 -0500

    Initial commit

---
+++ python-folder/class-jkb2.py
@@ -0,0 +1,44 @@
+"""
+I want to define a class named JKB()
+I want to define 2 class methods inside JKB
+I want to print out these class methods
+
+"""
+print('\nThis script prints out 2 class methods and attributes')
+
+class JKB:
+    def moron(self):
+        print("\nLe Departement de la Grand-Anse a pour chef-lieu: 'Jeremie'")
+    def leon(self):
+        print("L'arrondissement de Jeremie a 4 communes.'")
+    def marfranc(self):
+        print("L'arrondissement de Jeremie a 4 communes.'")
+
+    def bonbon(self):
+        print("L'arrondissement de Jeremie a 4 communes.'")
+
+
+
+darline = JKB()
+
+darline.moron()
+
+print()
+
+amelie = JKB()
+
+amelie.leon()
+
+print()
+
+yvette = JKB()
+
+yvette.marfranc()
+
+print()
+
+yvon = JKB()
+
+yvon.bonbon()
+
+print()
