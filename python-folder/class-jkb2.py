b391de0ce1ffb7b715f3987b2515907679a57277
jean sanon <jeansanon180@gmail.com>
2020-02-17 17:06:08 -0500

    2nd commit

--- python-folder/class-jkb2.py
+++ python-folder/class-jkb2.py
@@ -1,21 +1,24 @@
 """
 I want to define a class named JKB()
-I want to define 2 class methods inside JKB
+I want to define 4 class methods inside JKB
 I want to print out these class methods
 
 """
-print('\nThis script prints out 2 class methods and attributes')
+print('\nThis script prints out 4 class methods and attributes')
 
 class JKB:
+
     def moron(self):
-        print("\nLe Departement de la Grand-Anse a pour chef-lieu: 'Jeremie'")
+        print("\nLe Departement de la Grand-Anse est divise en 3 arrondissements:\n'Jeremie', 'Anse Dainault', 'Corail'")
+
     def leon(self):
-        print("L'arrondissement de Jeremie a 4 communes.'")
+        print("L'arrondissement de Jeremie a 6 communes:\n'Jeremie','Abricots','Bonbon','Chambellan','Mafranc','Moron''")
+
     def marfranc(self):
-        print("L'arrondissement de Jeremie a 4 communes.'")
+        print("L'arrondissement de Anse-Dainault a 3 communes:\n'Anse-Dainault','Dame-Marie','Les Irois''")
 
     def bonbon(self):
-        print("L'arrondissement de Jeremie a 4 communes.'")
+        print("L'arrondissement de Corail a  4 communes:\n'Corail','Beaumont','Pestel','Roseaux'")
 
 
 
