a0d26fa6221a4a7a4bd5ce7305cc4a8d734158b1
jean sanon <jeansanon180@gmail.com>
2020-02-19 16:51:14 -0500

    Initial commit

---
+++ python-folder/class-setters.py
@@ -0,0 +1,23 @@
+"""
+
+
+"""
+
+class Dog:
+#    name = 'toussaint pierre'.title()
+    name = ''
+
+    def setName(self, new_name):
+        self.name = new_name # declaring new value for name attribute
+
+    def getName(self): # returning value of name attribute
+        return self.name
+
+sam = Dog()
+
+sam.setName("gregory")
+#sam.setName("paul")
+
+print(sam.getName().title())
+
+print()
