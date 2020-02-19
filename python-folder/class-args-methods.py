9f458581fb79d7f58cad57f1290c243712882cfb
jean sanon <jeansanon180@gmail.com>
2020-02-18 20:54:20 -0500

    2nd commit

--- python-folder/class-args-methods.py
+++ python-folder/class-args-methods.py
@@ -4,11 +4,14 @@ I want to define a class method accepting parameters
 I want to define a class named Dog
 I want to define a class named Dog
 """
-print('\nThis script prints out argument that was passing to class method')
+print('\nThis script prints out 2 arguments that was passing to 2 class methods.\n')
 
 class Dog:
     def showAge(self, age):
-        print('Ada age is: {} years old'.format(age))
+        print('Ada age is: {} years old...'.format(age))
+
+    def showNationality(self, nationality):
+        print('Okinawa nationality is: {}...'.format(nationality).title())
 
 ada = Dog()
 
@@ -16,6 +19,8 @@ ada.showAge(22)
 
 print()
 
-#okinawa = Dog()
+okinawa = Dog()
 
-#okinawa.showAge(44)
+okinawa.showNationality('nigerian')
+
+print()
