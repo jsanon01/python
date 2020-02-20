2f998d4e9951248a51e0e6ee6f1503036e589bda
jean sanon <jeansanon180@gmail.com>
2020-02-20 11:47:56 -0500

    2nd commit

--- python-folder/setters-getters.py
+++ python-folder/setters-getters.py
@@ -18,4 +18,27 @@ avocado = Veggies()
 
 avocado.setName('carrots')
 
-print('{} are good for vision...'.format(avocado.getName().title()))
+print('\nThis script prints out setters and getters from a class')
+
+print('\nHaving carotene, {} are not only good for vision\nbut also for pigmentation.'.format(avocado.getName().title()))
+
+print()
+
+print('-------------------------------------------------------------')
+
+class Vitamins:
+    vitamin = ''
+    def setVitae(self, b_12):
+        self.vitamin = b_12
+    def getVitae(self):
+        return self.vitamin
+
+vitamin_e = Vitamins()
+
+vitamin_e.setVitae('vitamin b-12')
+
+print('\nThis script prints out setters and getters from a class')
+
+print('\n{} is a water soluble vitamin\nthat is naturally present in some foods'.format(vitamin_e.getVitae().title()))
+
+print()
