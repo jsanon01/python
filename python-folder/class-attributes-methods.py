cf1fb946615c44287d6b38db265acd7aa8135fe9
jean sanon <jeansanon180@gmail.com>
2020-02-27 21:54:57 -0500

    2nd commit

--- python-folder/class-attributes-methods.py
+++ python-folder/class-attributes-methods.py
@@ -1,9 +1,10 @@
 """
-I want to define 2 classes separately
+I want to define 2 classes Cat and Dog respectively
 I want to create 2 class methods inside of them
 I want to print them out
 
 """
+print('\nThis script is from class Cat(): ')
 
 class Cat:
     age = 10
@@ -16,6 +17,7 @@ kitty.anniversaire()
 
 print('Kitty is {} year-old.'.format(kitty.age).title())
 
+print('\nThis script is from class Dog(): ')
 
 class Dog:
     age = 10
@@ -26,5 +28,7 @@ sam = Dog()
 
 sam.happyBirthday()
 
-print('Sam age is {} year-old'.format(sam.age))
+print('Sam age is {} year-old.'.format(sam.age).title())
+
+print()
 
