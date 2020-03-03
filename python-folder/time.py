15f17506bee6b94dc1f43d751546289e3572597a
jean sanon <jeansanon180@gmail.com>
2020-03-02 21:28:40 -0500

    Initial commit

---
+++ python-folder/time.py
@@ -0,0 +1,15 @@
+"""
+
+
+"""
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H: %M: %S")
+
+
+print('------------------------------------')
+print('Current time  = ', current_time )  
+print('------------------------------------')
+
