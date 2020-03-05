edee3327b734ee219edcde4561d8c822d81f25e5
jean sanon <jeansanon180@gmail.com>
2020-03-04 22:01:18 -0500

    2nd commit

--- python-folder/class-magic-methods.py
+++ python-folder/class-magic-methods.py
@@ -1,24 +1,28 @@
 """
-
-the magic method is: __str__
+I want to define 2 classes named Cat and Lion repectively
+I want to define the magic method  __str__ for both classes
+I want to use 'return' statement in magic method for both classes
 
 """
+print("\nThis script prints out 2 classes using the magic method: '__str__'")
 
 class Cat:
     def __str__(self):
-        return 'this is a kitty'.title()
+        return '\nthis is a kitty...'.title()
 
 sam = Cat()
 
 print(sam)
 
-print('--------------------------')
+print('\n---------------------------------------------------')
 
 class Lion:
     def __str__(self):
-        return "the lion of juda's tribe"
+        return "\nthe lion of juda's tribe...".title()
 
 kiki = Lion()
 
 print(kiki)
+
+print()
 
