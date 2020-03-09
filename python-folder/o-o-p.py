2d730651f551001494c69a98c9b7c8bb6a0b35f7
jean sanon <jeansanon180@gmail.com>
2020-03-08 21:22:26 -0400

    2nd commit

--- python-folder/o-o-p.py
+++ python-folder/o-o-p.py
@@ -4,7 +4,14 @@ I want to define a function called class
 I want to define a function called instans
 I want to define a function called methods
 
-I want to define a main function
+I want to define a main function that will
+- have a condition statement with while loop
+- exit the loop when x = 0
+- call oop() when x = 1
+- call klass() when x = 2
+- call instans() when x = 3
+- call methods() when x = 4
+
 
 """
 print('\nOOP is not just supported by Python, but it is a part of its very core.')
@@ -22,7 +29,7 @@ def methods():
     print('instances use methods defining in the class')
 
 def main():
-    x = int(input('\nEnter inputs: '))
+    x = int(input('\nEnter a number from 0 - 4: '))
     while x:
         if x == 1:
             oop()
@@ -34,7 +41,7 @@ def main():
             methods()
         else:
             print('Invalid number..')
-        x = int(input('\nEnter inputs: '))
+        x = int(input('\nEnter a number from 0 - 4: '))
 
 main()
 
