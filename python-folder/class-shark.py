559818c089792346b012fb8261f75a7170304928
jean sanon <jeansanon180@gmail.com>
2020-03-09 21:32:59 -0400

    Initial commit

---
+++ python-folder/class-shark.py
@@ -0,0 +1,29 @@
+"""
+
+
+"""
+
+class Shark: # defining class
+    def swim(self): # defining function
+        print('the shark is swimming...'.title())
+
+    def awesome(self): # defining function
+        print('the shark is awesome...'.title())
+
+
+# object is an instance of a class
+sammy = Shark() # Shark object is called sammy (or we can say we initialized the object)
+sammy.swim() # method used with Shark object
+sammy.awesome() # method used with Shark object
+
+"""
+--------- good OPTIONAL coding ------------
+def main():
+    # object is an instance of a class
+    sammy = Shark() # Shark object is called sammy (or we can say we initialized the object)
+    sammy.swim() # method used with Shark object
+    sammy.awesome() # method used with Shark object
+
+main()
+
+"""
