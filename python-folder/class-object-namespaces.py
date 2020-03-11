738e220843c742bdf7e5c243d22d1331eb9b3329
jean sanon <jeansanon180@gmail.com>
2020-03-10 21:58:44 -0400

    2nd commit

--- python-folder/class-object-namespaces.py
+++ python-folder/class-object-namespaces.py
@@ -3,7 +3,7 @@ I want to define a class named Person
 I want to define a class attribute named species
 I want to print the value of class attribute
 
-I want to define a class named Person
+I want to instantiate 
 I want to define a class named Person
 
 """
@@ -11,16 +11,22 @@ class Person:
     species = 'equal'
 
 # printing the value inherited from the class attribute
-print('all men are created {}...'.format(Person.species).upper())
+print('\nall men are created {}...'.format(Person.species).upper())
 
 # accessing the class
 Person.alive = True
-print('we only live once: {}'.format(Person.alive))
+
+print('\nwe only live once: {}'.format(Person.alive).title())
 
 # instantiating
 man = Person()
-print('are all men created {}?'.format(Person.species))
 
-man.firstname = 'ti paul'
+print('\nare all men created {}?'.format(Person.species).upper())
+
+man.firstname = 'pierre-paul'
+
 man.lastname = 'valcourt'
-print('first name: {}\tlast name: {}'.format(man.firstname, man.lastname))
+
+print('\nfirst name: {}\tlast name: {}'.format(man.firstname, man.lastname).title())
+
+print()
