658ad5e37cbbe879e222d194f4e4746ef37de874
jean sanon <jeansanon180@gmail.com>
2020-03-11 21:19:46 -0400

    Inital commit

---
+++ python-folder/class-constructor-method.py
@@ -0,0 +1,31 @@
+"""
+
+
+"""
+
+class Shark:
+    def __init__(self):
+            print('\nconstructor method is used to initialize data...')
+            print("\nthe 1st definition of a class, it always looks like this:\nwhen using the init method... '__init__(self):\n ")
+    
+Shark()
+
+print('----------------Another Example----------------------')
+
+class Dauphin:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def swim(self):
+        print(self.fname + ' is swimming')
+
+    def bouncing(self):
+        print(self.lname +  ' is bouncing in the water.')
+
+kiki = Dauphin("leroi", 'polo')
+
+kiki.swim()
+
+kiki.bouncing()
+
