65bdeb61f1b613f069180e13664941cbc64bcd20
jean sanon <jeansanon180@gmail.com>
2020-03-12 20:59:23 -0400

    Initial commit

---
+++ python-folder/class-shark2.py
@@ -0,0 +1,24 @@
+"""
+
+"""
+
+class Shark:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def swim(self):
+        print(self.name + ' is swimming')
+
+    def awesome(self):
+        print(self.name + ' is pretty.')
+
+def main():
+    sammy = Shark('ginadelle', 6)
+
+    sammy.swim()
+    sammy.awesome()
+
+#    if __name__ == '__main__':
+
+main()
