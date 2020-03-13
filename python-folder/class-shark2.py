d6811fe53ea115a3c75f8ea26f3968103c24f9d3
jean sanon <jeansanon180@gmail.com>
2020-03-12 21:14:03 -0400

    2nd commit

--- python-folder/class-shark2.py
+++ python-folder/class-shark2.py
@@ -1,5 +1,13 @@
 """
+I want to define a class named Shark
+I want to initialize name and age using __init__
+I want to define a class method named swim
+I want to define a class method named awesome
 
+I want to define a main loop with the following:
+- creating an instance in class  Shark
+- calling swim method
+- calling awesome method
 """
 
 class Shark:
@@ -8,17 +16,20 @@ class Shark:
         self.age = age
 
     def swim(self):
-        print(self.name + ' is swimming')
+        print(self.name + ' is swimming'.title())
 
     def awesome(self):
-        print(self.name + ' is pretty.')
+        print(self.name + ' is pretty.'.title())
 
 def main():
-    sammy = Shark('ginadelle', 6)
-
+    sammy = Shark('ginadelle'.upper(), 7)
+    print()
     sammy.swim()
+    print()
     sammy.awesome()
 
-#    if __name__ == '__main__':
+#if __name__ == '__main__':
 
 main()
+
+print()
