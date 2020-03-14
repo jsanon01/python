67590b44c6dc1ec0f9ca860f9cb8098f081d6eb0
jean sanon <jeansanon180@gmail.com>
2020-03-13 21:42:41 -0400

    Initial commit

---
+++ python-folder/class-person.py
@@ -0,0 +1,28 @@
+"""
+
+
+"""
+
+class Person:
+    sport = "the state sports' minister"
+    sports = 'the state secretary of sports'
+    player = 'the most popular sports in haiti'
+
+charlot = Person()
+
+print('\nCharlot Jacklin Jr is {}'.format(charlot.sports).title())
+print()
+attys = Person()
+
+print('Max Attys is {}'.format(attys.sport).title())
+print()
+soccer = Person()
+
+print('soccer is {}'.format(soccer.player))
+
+print()
+
+man = Person()
+man.name = 'lamartine cleromnt'
+
+print(man.name)
