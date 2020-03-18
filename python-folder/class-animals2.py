78116320ba7125bdd9be4414873a56f5da97efae
jean sanon <jeansanon180@gmail.com>
2020-03-17 20:14:41 -0400

    2nd commit

--- python-folder/class-animals2.py
+++ python-folder/class-animals2.py
@@ -2,11 +2,13 @@
 I want to create a class named Animals
 I want to create a class attribute named species
 I want to create setter and getter to access attribute value
+
+I want to define a main loop
 I want to create an instance called lion
 I want to call the setter method with feline as argument
 I want to print out species by calling getter method
-
 """
+
 class Animals:
     def __init__(self, species):
         self.species = species
@@ -18,6 +20,10 @@ class Animals:
         return self.species
 
 species = ''
-lion = Animals(species)
-lion.setAnimal('le lion de la tribu de juda'.upper())
-print(lion.getAnimal().title())
+
+def main():
+    lion = Animals(species)
+    lion.setAnimal('le lion de la tribu de juda'.upper())
+    print(lion.getAnimal().title())
+
+main()
