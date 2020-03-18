487a133189af819d8a452dbe497172f02def08b9
jean sanon <jeansanon180@gmail.com>
2020-03-18 16:21:18 -0400

    Initial commit

---
+++ python-folder/top-5-languages.py
@@ -0,0 +1,31 @@
+"""
+I want to create a function named ruby
+I want to create a function named swift
+I want to create a function named java
+I want to create a function named python
+I want to create a function named javascript
+I want to create a function named ruby
+
+"""
+
+def ruby():
+    print('ruby on rails is suitable framewok bor building websites'.title())
+
+def swift():
+    print('swift is the primary language for building ios apps: iphones, ipads.\nthe downsize is not really a cross-platform language...'.title())
+
+def java():
+    print('java is one of the most powerful and widely used languages.\njava can also be used for creating android apps and websites...\nunfortunately it is not the easiest language to learn...')
+
+def python():
+    print('python is also one of the most popular programming languages.\nyou can create websites with django and flask frameworks.\nPython is also suitable for data sciences and machine learning...\noreover, python is popular at both small and larger companies like google')
+
+
+def javascript():
+    print('used to be a language running on web browsers, recently peple start using javascript to create backend codes running on servers.\nalso it is a language that is easy to learn.')
+
+ruby()
+swift()
+java()
+python()
+javascript()
