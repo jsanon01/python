96f4a7f52e4ccaf45d338f47b859282da6df70e0
jean sanon <jeansanon180@gmail.com>
2020-03-19 15:35:30 -0400

    Initial commit

---
+++ python-folder/javascript.py
@@ -0,0 +1,18 @@
+"""
+
+
+"""
+class Javascript:
+
+    def frontend(self):
+        print('\njavascript is interactive on the front-end side (users)'.upper())
+
+    def backend(self):
+        print('\njavascript is static on the back-end side (servers).'.upper())
+
+
+js = Javascript()
+
+js.frontend()
+js.backend()
+print()
