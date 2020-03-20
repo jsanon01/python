281c10f5bf4e5027644c48b24e7ada892feed6eb
jean sanon <jeansanon180@gmail.com>
2020-03-20 10:54:22 -0400

    Inital commit

---
+++ python-folder/dynamo-db.py
@@ -0,0 +1,44 @@
+"""
+
+
+"""
+
+def dynamodb():
+   print("\nhere are the most common use cases for dynamo db:")
+
+def ad_tech():
+    print("\n- user events and clickstreams\t- impressions data store.\n- metadata store for assets\t- popular-item caches\n- user profile stores in rtb and targeting.")
+
+def gaming():
+    print("\n- game states\t- player data stores\n- leaderboards\t- player sessionnhistory data stores")
+
+def retail():
+    print("\n- shopping carts\t- workflow engines\n- inventory tracking\t- customer profile and accounts")
+
+def banking():
+    print("\n- user transactions\t- fraud detection\n- event-driven transaction processing\t- mainframe offloading & change data capture")
+
+def media():
+    print("\n- media metadata stores\t- user data stores\n- digital rights management data stores")
+
+def os_and_internet():
+    print("\n- ride-tracking data stores\t- metadata caches\n- user content metadata stores\t- relationship graph data stores\n- user, vehicle and driver data stores")
+
+
+retail()
+ad_tech()
+gaming()
+media()
+os_and_internet()
+
+
+"""
+def webapp():
+    print(severless web apps)
+
+def mobile():
+    print(mobile backends)
+
+def microservices():
+    print()
+"""
