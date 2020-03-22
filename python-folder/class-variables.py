fb2318117ab54186cf42f94e2b4a3107199bf347
jean sanon <jeansanon180@gmail.com>
2020-03-22 14:49:18 -0400

    Initial commit

---
+++ python-folder/class-variables.py
@@ -0,0 +1,15 @@
+"""
+
+
+"""
+
+
+class Shark:
+    animal = 'tilapia'
+    location = 'ocean atlantic'
+    followers = 5
+
+new_shark = Shark()
+print(new_shark.animal)
+print(new_shark.location)
+print(new_shark.followers)
