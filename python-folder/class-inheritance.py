88818de10c59eb6b0d17cc6cac5f5b0efdf06652
jean sanon <jeansanon180@gmail.com>
2020-03-21 21:46:05 -0400

    2nd commit

--- python-folder/class-inheritance.py
+++ python-folder/class-inheritance.py
@@ -4,25 +4,36 @@ I want to define __init__ method with first_name and last_name
 I want to define another method named swim()
 I want to define a child class named swim_backwards()
 
-
+I want to define a child class named Trout() or object
+I want to create a Trout object named terry that will use Fish class
 """
 
 class Fish:
-    def __init__(self, first_name, last_name='fish'):
+    def __init__(self, first_name, last_name='Fish'):
         self.first_name = first_name
         self.last_name = last_name
 
     def swim(self):
-        print('the fish swims smoothly...')
+        print('\nthe fish swims smoothly...'.title())
 
     def swim_backwards(self):
-        print('all fish can swim backwards...')
+        print('\nall fish can swim backwards...'.upper())
 
 
 
 class Trout(Fish):
     pass
 
-terry = Trout('terry'.upper())
+print()
 
-print(terry.first_name)
+terry = Trout('terry'.title())
+
+print(terry.first_name, terry.last_name)
+
+#print(terry.first_name + ' ' + terry.last_name) # GOOD WORKING CODE
+
+terry.swim()
+
+terry.swim_backwards()
+
+print()
