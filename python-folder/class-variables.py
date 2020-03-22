684ed2fe52aebc914ef5502491bf8c0ea2f29dfd
jean sanon <jeansanon180@gmail.com>
2020-03-22 15:00:03 -0400

    2nd commit

--- python-folder/class-variables.py
+++ python-folder/class-variables.py
@@ -1,15 +1,24 @@
 """
+I want to define a class named Shark
+I want to define a class variable named animal
+I want to define a class named location
+I want to define a class named followers
 
-
+I want to print all the class variables in one line
 """
 
 
 class Shark:
-    animal = 'tilapia'
+    animal = 'dolphins'
     location = 'ocean atlantic'
     followers = 5
 
-new_shark = Shark()
-print(new_shark.animal)
-print(new_shark.location)
-print(new_shark.followers)
+new_shark = Shark() # instantiating new_shark
+
+#print(new_shark.animal) # class varaible (GOOD CODING WILL PRINT SEPARATELY)
+
+#print(new_shark.location) # class variable (GOOD CODING WILL PRINT SEPARATELY)
+
+#print(new_shark.followers) # class variable (GOOD CODING WILL PRINT SEPAARATELY)
+
+print('\n{} often go to the {} with {} followers.\n'.format(new_shark.animal, new_shark.location, new_shark.followers).title())
