418ba399b499d878849619adfadb0796524ced1f
jean sanon <jeansanon180@gmail.com>
2020-03-23 12:27:29 -0400

    Initial commit

---
+++ python-folder/class-variables-instances.py
@@ -0,0 +1,32 @@
+"""
+I want to define a class named Shark
+I want to define a construct method with 2 instance variables: name and age
+I want to define a class method named jeremie
+I want to instantiate the variables
+
+I want to print all the instance variables in one line
+"""
+
+# defining a class
+class Shark:
+    
+    # defining construct method with 2 instance variables
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # defining a method named jeremie with instance variable named location
+    def jeremie(self, location):
+        self.location = location
+
+# Instantiating meaning assigning piskette as instance
+piskette = Shark('piskette', 9)
+
+# printing out 2 instance varaibles
+print('the object: piskette = passes the parameters --> {} and {}'.format(piskette.name, piskette.age))
+
+# Instantiating meaning assigning sou_fort as instance
+sou_fort = Shark('kiki', 4)
+
+# printing out 2 instance variables
+print(sou_fort.name, sou_fort.age)
