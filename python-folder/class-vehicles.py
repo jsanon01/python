4a59387e609c1449f0e7f466cade3f732febf2e4
jean sanon <jeansanon180@gmail.com>
2020-03-26 19:56:38 -0400

    Initial commit

---
+++ python-folder/class-vehicles.py
@@ -0,0 +1,17 @@
+"""
+
+
+"""
+
+
+class Vehicle:
+    # defining class variables
+    name = 'bmw'.upper()
+    kind = 'convertible'.title()
+    color = 'red'.title()
+    value = 100000.00
+
+car1 = Vehicle()
+# instance variables in 1 line
+print('\nHere is the spec:\n{} {} {} {}\n '.format(car1.name, car1.kind, car1.color, car1.value))
+#print()
