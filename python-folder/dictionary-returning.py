0086d4dc2f1c949e20e8b7cb540c592ec51efc9b
jean sanon <jeansanon180@gmail.com>
2020-03-27 17:04:59 -0400

    2nd commit

--- python-folder/dictionary-returning.py
+++ python-folder/dictionary-returning.py
@@ -1,13 +1,19 @@
 """
-
+I want to define a function with 2 values
+I want to define a variable with a dictionary as values
+I want to instantiate an object named musician
+I want to print the object name musician
 
 """
 
-
-def build_person(first_name, last_name):
-    person = {'first': first_name, 'last': last_name}
+def build_person(first_name, last_name, age):
+    person = {'First': first_name, 'Last': last_name, 'Age': age}
     return person
 
-musician = build_person('eric', 'laguerre')
+musician = build_person('eric'.title(), 'laguerre'.upper(), 61)
+
+print()
 
 print(musician)
+
+print()
