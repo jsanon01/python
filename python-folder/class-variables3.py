0c4475a87f969d8cda79a9be9ded42f235f4d002
jean sanon <jeansanon180@gmail.com>
2020-03-28 16:10:50 -0400

    Initial commit

---
+++ python-folder/class-variables3.py
@@ -0,0 +1,35 @@
+"""
+I want to define a class named Shark
+I want to define a constructor method with 2 class variables: name and age
+I want to define another class variable named followers
+
+I want to define a function name main()
+I want to define an instance variable named sammy
+I want to print out the instance variable named sammy
+https://www.digitalocean.com/community/tutorials/understanding-class-and-instance-variables-in-python-3
+"""
+
+#print(Here is )
+# defining a class 
+class Shark:
+
+    # defining a constructor with 2 instance variables: name and age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # defining a method with instance variable followers
+    def set_followers(self, followers):
+        self.followers = followers
+
+def main():
+
+    # 1st object, instance variable of constructor method
+    sammy = Shark('sammy',6)
+    jeremie = Shark('piskette', 24)
+
+    # printing out intance variable
+    print('{} is {} year-old.'.format(sammy.name.upper(), sammy.age))
+    print('{} come out around August {} of each year.'.format(jeremie.name.upper(), jeremie.age))
+
+main()
