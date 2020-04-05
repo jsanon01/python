f57c0faf07b5799c3113f2f913926323c6c3519d
jean sanon <jeansanon180@gmail.com>
2020-04-04 22:34:15 -0400

    Initial commit

---
+++ deep-thinking.py
@@ -0,0 +1,22 @@
+"""
+I want to define a function named low_level
+I want to define a function named mid_level
+I want to define a function named low_level
+I want to define a function named high_level
+I want to define a function named steps
+
+I want to define a main function
+
+
+"""
+def low_level():
+    print("'Low level' copes with relaxation, sleeping, and ezternal senses...")
+
+def mid_level():
+    print("'Mid Level' uses focusing or reasoning...")
+
+def high_level():
+    print("'High level' has to do with envision, creativity, visualizing, imagining")
+
+def steps():
+    print('those steps are as followed:\nLow level can meet Mid level.\nMid-Level can meet High Level.\nThus, the extremities can not meet.')
