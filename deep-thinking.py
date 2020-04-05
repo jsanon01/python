a5754aa14ecfa4e469014f433dd02e40593d3418
jean sanon <jeansanon180@gmail.com>
2020-04-04 22:45:54 -0400

    2nd commit

--- deep-thinking.py
+++ deep-thinking.py
@@ -10,13 +10,24 @@ I want to define a main function
 
 """
 def low_level():
-    print("'Low level' copes with relaxation, sleeping, and ezternal senses...")
+    print("\n'Low level' copes with relaxation, sleeping, and external senses...")
 
 def mid_level():
-    print("'Mid Level' uses focusing or reasoning...")
+    print("\n'Mid Level' uses focusing or reasoning...")
 
 def high_level():
-    print("'High level' has to do with envision, creativity, visualizing, imagining")
+    print("\n'High level' has to do with envision, creativity, visualizing, imagining")
 
 def steps():
-    print('those steps are as followed:\nLow level can meet Mid level.\nMid-Level can meet High Level.\nThus, the extremities can not meet.')
+    print('\nThe mandatory steps are as followed:\n\nLow level can meet Mid level.\n\nMid-Level can meet High Level.\n\nThus, the extremities can not meet.')
+
+def main():
+    low_level()
+    mid_level()
+    high_level()
+    steps()
+
+
+main()
+    
+print()
