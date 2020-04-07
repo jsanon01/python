963c62163b4d00f310571f1515cf7048c25bda16
jean sanon <jeansanon180@gmail.com>
2020-04-07 13:32:06 -0400

    2nd commit

--- class-robot.py
+++ class-robot.py
@@ -1,19 +1,33 @@
 """
+I want to create a class named Robot
+I want to create a method named introduce_self
+I want to instantiate kiki as object inside the class named Robot
+I want to create the following class attributes:
+- name, color, weight
+I wamt to call or print out the method
+
 https://www.youtube.com/watch?v=wfcWRAxRVBA
 """
 
 class Robot:
     def introduce_self(self):
-        print("My name is {}".format(self.name))
+        print("\nMy name is {}".format(self.name))
+
+r1 = Robot()
+r1.name = 'junia pierre'.title()
+r1.color = 'white'
+r1.weight = 79
+r1.introduce_self()
+
 
 kiki = Robot()
-kiki.namee = 'tom'.upper()
+kiki.name = 'tom & jerry'.upper()
 kiki.color = 'red'
 kiki.weight = 40
 kiki.introduce_self()
-print()
+
 roro = Robot()
-roro.name = 'jerry'.upper()
+roro.name = 'vlad bazile'.upper()
 roro.color = 'blue'
 roro.weight = 70
 roro.introduce_self()
