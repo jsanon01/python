8d0fb009246e13afe4ae4f8f6a9914094476c3a1
jean sanon <jeansanon180@gmail.com>
2020-04-09 12:53:58 -0400

    Initial commit

---
+++ class-child2.py
@@ -0,0 +1,38 @@
+"""
+I want to define a class named Person
+I want to define a constructor method with self
+I want to define a method named info
+I want to create an instance variiable named ana
+
+"""
+
+class Person:
+    def __init__(self):
+        self.fname = input('Enter first name: ').title()
+        self.lname = input('Enter last name: ').title()
+        self.phone = input('Enter phone number: ')
+        self.address = input('Enter address: ').title()
+
+    def info(self):
+        print('\nFirst name: {}\tLast name: {}\tPhone: {}\tAddress: {}'.format(self.fname, self.lname, self.phone, self.address))
+
+ana = Person()
+ana.info()
+print()
+
+
+
+
+
+
+
+
+"""
+def main():
+#    ana = Person(' ',' ')
+    fname = input('enter first name: ')
+    lname = input('enter last name: ')
+#    ana = Person(' ',' ')
+
+main()
+"""
