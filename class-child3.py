1390212f8122b5c963d0e0d3c025de54f5f55ab2
jean sanon <jeansanon180@gmail.com>
2020-04-10 11:37:17 -0400

    2nd commit

--- class-child3.py
+++ class-child3.py
@@ -1,22 +1,27 @@
 """
-I want to 
+I want to define a class named Person
+I want to define a constructor method with 2 class attributes:
+- firstname and lastname
+I want to define a class method named phone
+I want to define a class method named show_full_name
+I want to create instance variable name person1
 
 """
 class Person:
 
     def __init__(self):
-        self.firstname = input('Enter first name:  ').title()
-        self.lastname = input('Enter last name:  ').upper() 
+        self.firstname = input('\nEnter first name:  ').title()
+        self.lastname = input('\nEnter last name:  ').upper() 
 
     def phone(self):
-        self.phone = int(input('enter phone number: '))
+        self.phone = int(input('\nEnter phone number: '))
 
     def show_full_name(self):
-        print('First name: {}\nLast name: {}\nPhone number: {}'.format(self.firstname, self.lastname, self.phone))
+        print('\nFirst name: {}\nLast name: {}\nPhone number: {}'.format(self.firstname, self.lastname, self.phone))
 #         print(self.firstname + ' ' + self.lastname) # GOOD CODING
 
 #creating an object with the class
 person1 = Person()
 person1.phone()
 person1.show_full_name()
-
+print()
