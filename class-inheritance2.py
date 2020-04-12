0e5747782252fc3fdbcf959ebb74bdedbb0eae51
jean sanon <jeansanon180@gmail.com>
2020-04-12 17:20:33 -0400

    2nd commit

--- class-inheritance2.py
+++ class-inheritance2.py
@@ -1,31 +1,38 @@
-
+"""
+I want to define a super-class named Parent
+I want to define a function-method named parent
+I want to define a sub-class named Child
+I want to define a function-method named child
+I want to instantiate an object
+I want to call both methods: parent and child
+"""
 
 
 class Parent:
     def parent(self):
-        print('\nThis a parent class')
+        print('\nThis a parent class --> class Parent: ')
 
 class Child(Parent):
     def child(self):
-        print('\nThis is a child class')
+        print('\nThis is a child class --> class Child(Parent):')
 
-#object = Parent()
-#object.parent()
 
 object = Child()
 object.parent()
 object.child()
+
 print('\n--------------------------------------------')
 
 class Karlo:
     def children(self):
-        print('\nKaro and Kris')
+        print('\nclass Karlo: Karo and Kris.')
 
 class Roseline(Karlo):
     def child(self):
-        print('\nKerline aussi')
+        print('\nclass Roseline(Karlo): Kerline aussi.')
 
 enfants = Roseline()
 enfants.children()
 enfants.child()
+
 print()
