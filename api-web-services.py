01d49d29134238a35cb084ef9b851c8f364e2073
jean sanon <jeansanon180@gmail.com>
2020-04-15 22:00:20 -0400

    Initial commit

---
+++ api-web-services.py
@@ -0,0 +1,38 @@
+"""
+https://www.youtube.com/watch?v=B3-BEY7Mv_8
+https://www.upwork.com/search/profiles/?nbs=1&q=cloud&profile=~0107cd83ec37495694
+
+"""
+print()
+
+def type():
+    print('\n2 types of api web services:\n- soap which is built on different cross-platforms\n- restful which works with all media components, files, or objects.'.upper())
+
+
+def soap():
+    print('SOAP => simple object access protocol'.title())
+    print('\nmedium: http (post)\t\t\tformat: xml'.upper())
+
+def rest():
+    print('\nREST => representational state transfer'.title())
+    print('\nmedium: http (post, get, put, delete)\tformat: xml/json/text'.upper())
+
+def client():
+    print('\nCLIENT => is service consumer | Always REQUEST')
+    print('\nmedium: http/internet\t\t\tformat: xml/json'.upper())
+
+def server():
+    print('\nSERVER => is service provider | Always RESPOND')
+    print('\nmedium: http/internet\t\t\tformat: xml/json'.upper())
+
+
+def main():
+    type()
+    print()
+    soap()
+    rest()
+    client()
+    server()
+
+main()
+print()
