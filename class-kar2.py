3794b631f21522aefc31eaf3b0ee0fae8ad9f454
jean sanon <jeansanon180@gmail.com>
2020-04-19 20:31:30 -0400

    2nd commit

--- class-kar2.py
+++ class-kar2.py
@@ -24,10 +24,10 @@ class Car:
         print('\nYou have entered: {} {} {}'.format(self.make, self.model, self.year).title())
 
     # creating method
-#    def mileage(self):
-#        self.mileage = int(input('\nEnter the mileage: '))
-#        self.mileage += 1000
-#        print("\nThis car has {} miles on it.".format(self.mileage))
+    def mileage(self):
+        self.mileage = int(input('\nEnter the mileage: '))
+        self.mileage += 1000
+        print("\nThis car has {} miles on it.".format(self.mileage))
 
 
 
