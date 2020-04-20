a94d11b1f481c1c5e6d970e54b91422a6d448b0e
jean sanon <jeansanon180@gmail.com>
2020-04-20 19:57:36 -0400

    3rd commit

--- big-data.py
+++ big-data.py
@@ -14,11 +14,24 @@ def examples():
     print('\nvarious applications used by big data.'.title())
     print('\nweb\tfinance\thealthcare\tindustry\tenvironment\niot\tretail\ttransportation')
 
+print('\n[0] Quit [1] web    [2] finance [3] healthcare [4] industry [5] environment\n[6] iot   [7] retail [8] transportation')
+
+def web():
+    print('web analytics deals with user visits related to online ad campaigns.\nperformance monitoring deals with vertical or horizintal scaling.\nad targeting deals with search and display advertisements.\ncontent recommendation deals with user search patterns, ratings, and browser histories.')
+
+def finance():
+    print('\credit risk modeling ')
 
 def main():
     big_data()
     characteristics()
     examples()
+    x = int(input('enter a number: '))
+    if x == 1:
+        web()
+    elif x ==2:
+        web()
+
 
 main()
 print()
