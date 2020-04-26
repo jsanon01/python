6930b12d90138be465758c4d5ea70a5c28fa2fd0
jean sanon <jeansanon180@gmail.com>
2020-04-26 10:28:22 -0400

    Initial commit

---
+++ polymorphism.py
@@ -0,0 +1,19 @@
+"""
+
+
+"""
+
+# length being used for a string
+print("length being used for a string: 'massachusetts'".title())
+print(len('massachusetts'))
+
+# length of numbers being used for string
+print("length of numbers being used for a string: '0,1,2,3,4,5' ".title())
+print(len('0,1,2,3,4,5'))
+
+# length being used for a list
+print('length of numbesr being used for a list: [0,1,2,3,4,5] '.title())
+print(len([0,1,2,3,4,5]))
+
+#print(len('massachusetts'))
+print()
