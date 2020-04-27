d65af8e8d7eb4c0e4069626e4ce572d576280150
jean sanon <jeansanon180@gmail.com>
2020-04-27 19:24:44 -0400

    2nd commit

--- polymorphism-functions.py
+++ polymorphism-functions.py
@@ -1,12 +1,18 @@
 """
+I want to define a function named add
+I want to define a function named sub 
+
 https://www.geeksforgeeks.org/polymorphism-in-python/
 
 """
 
+print("\nThis script prints out the sum of a function:\ndef add(x + y + z = 0):\n\treturn x + y + z\n\nprint(add(2, 9))\nprint(add(2, 6, 9) ")
+print()
 
 def add(x, y, z = 0):
     return x + y + z
 
+print("\nThis script prints out the difference of a function:\ndef add(x - y - z = 0):\n\treturn x - y - z\n\nprint(add(2, 9))\nprint(add(2, 6, 9) ")
 def sub(x, y, z = 0):
     return x - y - z
 
@@ -17,3 +23,4 @@ print()
 
 print(sub(12, 45))
 print(sub(72, 39, 15))
+print()
