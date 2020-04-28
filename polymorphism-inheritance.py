99e3e6c2a2a227e44308ce1fd0c530a2b934c374
jean sanon <jeansanon180@gmail.com>
2020-04-28 19:32:38 -0400

    2nd commit

--- polymorphism-inheritance.py
+++ polymorphism-inheritance.py
@@ -1,5 +1,11 @@
 """
-
+I want to create a class Bird
+I want to create 3 functions inside the class Bird:
+- intro  - flight  - eat
+I want to create a child class named sparrow
+I want to create a child class named ostrich
+I want to make 3 object instances and call them:
+- kiki  - kika - kiko
 
 """
 
@@ -7,22 +13,22 @@ print('\n--------- polymorphism with inheritance -------------------'.title())
 
 class Bird:
     def intro(self):
-        print('\nthere are many types of birds.')
+        print('\nthere are many types of birds.'.title())
 
     def flight(self):
-        print('\nbirds can survive during their flight.')
+        print('\nbirds can survive during their flight.'.title())
 
     def eat(self):
-        print('\nall birds eat nuts.')
+        print('\nall birds eat nuts.'.title())
 
 
 class sparrow(Bird):
     def flight(self):
-        print('\nsparrow fly from north to south.')
+        print('\nsparrow fly from north to south.'.upper())
 
 class Ostrich(Bird):
     def flight(self):
-        print('\nostrich fly form east to west.')
+        print('\nostrich fly form east to west.'.upper())
 
 kiki = Bird()
 kika = sparrow()
@@ -32,7 +38,9 @@ kiko = Ostrich()
 kiki.intro()
 kiki.flight()
 kiki.eat()
+
 print('\n-------------------------------------------')
+
 kika.intro()
 kika.flight()
 kika.eat()
