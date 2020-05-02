4b32e3597a02f42c2739468087d58fa6c7951560
jean sanon <jeansanon180@gmail.com>
2020-05-01 21:14:46 -0400

    Initial commit

---
+++ lambda-functions.py
@@ -0,0 +1,14 @@
+
+double = lambda x: x * 2
+
+print("\nlambda function is used to print out 'the double of 5'".title())
+
+print(double(5))
+
+
+print("\nlambda function is used to print out 'the triple of 9'".title())
+
+triple = lambda x: x * 3
+
+print(triple(9))
+print()
