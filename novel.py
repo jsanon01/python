9766daedb8d12e49cf72d0e6f5134dd6197373cb
jean sanon <jeansanon180@gmail.com>
2020-05-05 22:13:21 -0400

    Initial commit

---
+++ novel.py
@@ -0,0 +1,82 @@
+"""
+the 3 act structure: setup conflict reolution
+
+"""
+print("\n'the save the cat beat sheet' is a roadmap which means\na series or road markers preventing aimlessly roaming.".title())
+
+print('\n[1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11] [12] [13] [14] [15] [16] ')
+
+def step_1():
+    print('step 1 --> opening image')
+def step_2():
+    print('step 2 --> setup ')
+def step_3():
+    print('step 3 --> theme stated')
+def step_4():
+    print('step 4 --> catalyst')
+def step_5():
+    print('step 5 --> debate')
+def step_6():
+    print('step 6 --> break into two')
+def step_7():
+    print('step 7 --> b story')
+def step_8():
+    print('step 8 --> fun & games')
+def step_9():
+    print('step 9 --> midpoint')
+def step_10():
+    print('step 10 --> bad guy close-in')
+def step_11():
+    print('step 11 --> all is lost')
+def step_12():
+    print('step 12 --> dark night of the soul')
+def step_13():
+    print('step 13 --> break into three')
+def step_14():
+    print('step 14 --> finale')
+def step_15():
+    print('step 15 --> final image')
+
+def main():
+    x = int(input('\nEnter a number from 0 - 16: '))
+    while x:
+        if x == 1:
+            step_1()
+        elif x == 2:
+            step_2()
+        elif x == 3:
+            step_3()
+        elif x == 4:
+            step_4()
+        elif x == 5:
+            step_5()
+        elif x == 6:
+            step_6()
+        elif x == 7:
+            step_7()
+        elif x == 8:
+            step_8()
+        elif x == 9:
+            step_9()
+        elif x == 10:
+            step_10()
+        elif x == 11:
+            step_11()
+        elif x == 12:
+            step_12()
+        elif x == 13:
+            step_13()
+        elif x == 14:
+            step_14()
+        elif x == 15:
+            step_15()
+        else:
+            print('\nInvalid number...')
+        x = int(input('\nEnter a number from 0 - 16: '))
+
+    print('\nYou have exited the script...')
+
+main()
+
+print()
+
