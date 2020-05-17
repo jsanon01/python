96a65a40670ff753d0b0291f2b14a649966f0e54
jean sanon <jeansanon180@gmail.com>
2020-05-17 15:12:51 -0400

    Initial commit

---
+++ role-csa.py
@@ -0,0 +1,46 @@
+"""
+
+
+"""
+
+
+def conducting():
+    print("conducting architectural evaluation")
+
+def ensuring():
+    print("ensuring on-time delivery")
+
+def identification():
+    print("identification of custmoer requirements")
+
+def working():
+    print("working with the sales department")
+
+def communicating():
+    print("communicating with system requirements")
+
+def overseeing():
+    print("overseeing assigned programs")
+
+def providing():
+    print("providing assistance")
+
+def monitoring():
+    print("monitoring systems")
+
+
+def main():
+    conducting()
+    ensuring()
+    identification()
+    working()
+    communicating()
+    overseeing()
+    providing()
+    monitoring()
+
+main()
+
+print()
+
+
