d5dd4a3b7406e422e453dd6836a5fc20b202443f
jean sanon <jeansanon180@gmail.com>
2020-05-31 15:14:25 -0400

    2nd commit

--- serverless4.py
+++ serverless4.py
@@ -4,16 +4,26 @@
 """
 
 def traditional():
-    print("traditional architecture: ".upper())
-    print("'- create & setup servers' - install os & db".title())
-    print('- managing software patches, capacity & scaling\n- hadrware updates'.title())
-    print('managing H.A. via load-balancing - vertical scaling'.title())
-    print('cost of infrastructure, computing power'.title())
+    print("\n---------- traditional architecture ----------\n ".upper())
+    print("- 'create & setup servers'\t- install os & db".title())
+    print('- managing software patches \t- managing  capacity & scaling\n- hadrware updates\t\t- vertical scaling'.title())
+    print('- cost of infrastructure \t- cost of computing power'.title())
+    print('- managing H.A. via load-balancing'.title())
+
 
 def serverless():
-    print('serverless architecture: '.upper())
-    print("no maintenance of high-end servers\- no worries of uptime or availability\- run on-demand \t- focus on writing codes".title())
-    print('underlying architecture is abstracted'.title())
+    print('\n---------- serverless architecture ----------\n '.upper())
+    print("- no maintenance of high-end servers\n- no worries of uptime or availability\n- run on-demand\n- being more productive \n- focus on writing codes".title())
+    print('- underlying architecture is abstracted'.title())
+    print()
+
+def computing():
+    print("---------- serverless computing ----------\n".upper())
+    print("--> it is the next generation of systems that can handle demanding\n\tworkloads witjout provisioning or managing any servers".title())
+    print()
+    print("--> it is the new trend in cloud computing which attemppts to\n\tsovle many of these challenges:\n- fast\t- responsive\t- scalable\t- no downtime".title())
+    print()
 
 traditional()
 serverless()
+computing()
