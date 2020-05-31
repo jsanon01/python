a9eb65e2b83819abba00ec59f98b64477328e499
jean sanon <jeansanon180@gmail.com>
2020-05-31 14:46:53 -0400

    Initial commit

---
+++ serverless4.py
@@ -0,0 +1,19 @@
+"""
+
+
+"""
+
+def traditional():
+    print("traditional architecture: ".upper())
+    print("'- create & setup servers' - install os & db".title())
+    print('- managing software patches, capacity & scaling\n- hadrware updates'.title())
+    print('managing H.A. via load-balancing - vertical scaling'.title())
+    print('cost of infrastructure, computing power'.title())
+
+def serverless():
+    print('serverless architecture: '.upper())
+    print("no maintenance of high-end servers\- no worries of uptime or availability\- run on-demand \t- focus on writing codes".title())
+    print('underlying architecture is abstracted'.title())
+
+traditional()
+serverless()
