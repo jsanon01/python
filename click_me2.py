bf29e02a95fd42ec6625e439a851ed10b6cf32f2
jean sanon <jeansanon180@gmail.com>
2020-06-04 11:29:39 -0400

    5th commit

--- click_me2.py
+++ click_me2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python3
 from tkinter import *
 #from tkinter import ttk
+import re
 
 root = Tk()
 root.title('New Application')
@@ -16,17 +17,22 @@ label1 = Label(root, text='Enter your full name: ', font=('arial', 15, 'bold'), 
 
 label2 = Label(root, text='Enter phone number: ', font=('arial', 15, 'bold'), fg='black').place(x=10, y=150)
 
+label3 = Label(root, text='Enter E-mail address: ', font=('arial', 15, 'bold'), fg='black').place(x=10, y=200)
+
 name = StringVar()
 phone = StringVar()
+email = StringVar()
 
 entry_box = Entry(root, textvariable=name, width=25, bg='lightgreen').place(x=170, y=100)
 entry_box2 = Entry(root, textvariable=phone, width=25, bg='lightgreen').place(x=170, y=150)
+entry_box3 = Entry(root, textvariable=email, width=25, bg='lightgreen').place(x=170, y=200)
 
 def do_it():
     print('Hello '+ str(name.get()).title())
-    print('Your phone number is: ' + str(phone.get()))
+    #print('Your phone number is: ' + str(phone.get()))
+    print("Phone number is: ",'(%s) %s-%s' % tuple(re.findall(r'\d{4}$|\d{3}', '0123456789')))
+    print('your e-mail address is: '.title())
 
-
-work = Button(root, text='Click Me', width=23, height=5, bg='lightblue', command=do_it).place(x=160, y=200)
+work = Button(root, text='Click Me', width=23, height=5, bg='lightblue', command=do_it).place(x=160, y=230)
 
 root.mainloop()
