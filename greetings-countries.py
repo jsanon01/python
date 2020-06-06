bf1e1d1dcdde597c6c2f17a9287338802932ecc5
jean sanon <jeansanon180@gmail.com>
2020-06-06 15:06:12 -0400

    2nd commit

--- greetings-countries.py
+++ greetings-countries.py
@@ -8,36 +8,35 @@ class HelloApp:
         self.label = ttk.Label(master, text='greetings!'.upper())
         self.label.grid(row = 0, column = 0, columnspan = 2 )
 
-        ttk.Button(master, text = 'texas!'.title(),
-                    command = self.texas_hello).grid(row=1, column=0)
-
-        ttk.Button(master, text = 'Hawaii!',
-                    command = self.hawaii_hello).grid(row=1, column=1)
-
-        ttk.Button(master, text = 'Haiti', command= self.haiti_hello).grid(row=2, column=0)
-
-        ttk.Button(master, text = 'Canada', command= self.canada_hello).grid(row=2, column=1)
-
-        ttk.Button(master, text = 'U.S.A.', command= self.america_hello).grid(row=3, column=0)
-
         ttk.Button(master, text = 'India', command= self.india_hello).grid(row=3, column=1)
 
+        ttk.Button(master, text = 'texas!'.title(), command = self.texas_hello).grid(row=2, column=1)
+
+        ttk.Button(master, text = 'Hawaii!', command = self.hawaii_hello).grid(row=3, column=0)
+
+        ttk.Button(master, text = 'Haiti', command= self.haiti_hello).grid(row=1, column=0)
+
+        ttk.Button(master, text = 'Canada', command= self.canada_hello).grid(row=2, column=0)
+
+        ttk.Button(master, text = 'U.S.A.', command= self.america_hello).grid(row=1, column=1)
+
+
         button_quit = Button(master, text = 'Exit Pprogram', command=master.quit).grid(row=4, column=0)
+
+    def haiti_hello(self):
+        self.label.config( text = 'Sak Pase Haiti!')
+
+    def america_hello(self):
+        self.label.config( text = 'Good Morning America!')
+
+    def canada_hello(self):
+        self.label.config( text= 'Bonjour Canada!')
 
     def texas_hello(self):
         self.label.config( text= "Howdy Texas!")
 
     def hawaii_hello(self):
         self.label.config( text= 'Aloha Hawaii!')
-
-    def haiti_hello(self):
-        self.label.config( text = 'Sak Pase Haiti!')
-
-    def canada_hello(self):
-        self.label.config( text= 'Bonjour Canada!')
-
-    def america_hello(self):
-        self.label.config( text = 'Good Morning America!')
 
     def india_hello(self):
         self.label.config( text = 'Namaste India!')
