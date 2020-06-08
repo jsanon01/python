b3ebfbce33a0e44729904c185ab76549339899bc
jean sanon <jeansanon180@gmail.com>
2020-06-07 21:07:58 -0400

    3rd commit

--- greetings-countries.py
+++ greetings-countries.py
@@ -10,9 +10,9 @@ class HelloApp:
 
         ttk.Button(master, text = 'India', command= self.india_hello).grid(row=3, column=1)
 
-        ttk.Button(master, text = 'texas!'.title(), command = self.texas_hello).grid(row=2, column=1)
+        ttk.Button(master, text = 'brasil'.title(), command = self.texas_hello).grid(row=2, column=1)
 
-        ttk.Button(master, text = 'Hawaii!', command = self.hawaii_hello).grid(row=3, column=0)
+        ttk.Button(master, text = 'Hawaii', command = self.hawaii_hello).grid(row=3, column=0)
 
         ttk.Button(master, text = 'Haiti', command= self.haiti_hello).grid(row=1, column=0)
 
@@ -21,7 +21,7 @@ class HelloApp:
         ttk.Button(master, text = 'U.S.A.', command= self.america_hello).grid(row=1, column=1)
 
 
-        button_quit = Button(master, text = 'Exit Pprogram', command=master.quit).grid(row=4, column=0)
+        button_quit = Button(master, text = 'Exit Program', command=master.quit).grid(row=4, column=0)
 
     def haiti_hello(self):
         self.label.config( text = 'Sak Pase Haiti!')
@@ -33,7 +33,7 @@ class HelloApp:
         self.label.config( text= 'Bonjour Canada!')
 
     def texas_hello(self):
-        self.label.config( text= "Howdy Texas!")
+        self.label.config( text= "bom dia Brasil!")
 
     def hawaii_hello(self):
         self.label.config( text= 'Aloha Hawaii!')
