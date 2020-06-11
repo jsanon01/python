4e4ee970f857c0581a0f945a4358a0f16a50ec16
jean sanon <jeansanon180@gmail.com>
2020-06-11 13:17:01 -0400

    Initial commit

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests
+
+api_address = 'https://api.openweathermap.org/data/2.5/weather?appid=426767ea170627f14a06a5979bea2de4&q='
+
+city = input('city name: '.title())
+unit = '&units=imperial'
+url = api_address + city + unit
+
+json_data = requests.get(url).json()
+json_data2 = requests.get(url).json()
+json_data3 = requests.get(url).json()
+json_data4 = requests.get(url).json()
+json_data5 = requests.get(url).json()
+json_data6 = requests.get(url).json()
+json_data7 = requests.get(url).json()
+json_data8 = requests.get(url).json()
+
+
+formatted_data = json_data['weather'][0]['main']
+formatted_data2 = json_data2['weather'][0]['description']
+formatted_data3 = json_data3['wind']['speed']
+formatted_data4 = json_data4['main']['humidity']
+formatted_data5 = json_data5['main']['temp']
+formatted_data6 = json_data6['main']['temp_min']
+formatted_data7 = json_data7['main']['temp_max']
+formatted_data8 = json_data8['main']['feels_like']
+
+
+print('Weather: {}\nDescription: {}\nWind speed: {}\nHumidity: {}'.format(formatted_data, formatted_data2, formatted_data3, formatted_data4).title())
+print('temp: {}\nTemp min: {}\ntemp max: {}\nfeel like: {}'.format(formatted_data5, formatted_data6, formatted_data7, formatted_data8).title())
