c395198df5277f25c49b56505929dda8de8c80d3
jean sanon <jeansanon180@gmail.com>
2020-06-10 20:53:08 -0400

    2nd commit

--- user-pass-form.py
+++ user-pass-form.py
@@ -6,24 +6,25 @@ def validateLogin(username, password):
     print("password entered :", password.get())
     return
 
-#window
+# setting up window-frame
 tkWindow = Tk()  
 tkWindow.geometry('400x150')  
 tkWindow.title('Tkinter Login Form - pythonexamples.org')
 
-#username label and text entry box
+# setting up username label and text entry box
 usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
 username = StringVar()
 usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  
 
-#password label and password entry box
+# setting up password label and password entry box
 passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
 password = StringVar()
 passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
 
 validateLogin = partial(validateLogin, username, password)
 
-#login button
+# setting up login button
 loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)  
 
+# exiting and running the program
 tkWindow.mainloop()
