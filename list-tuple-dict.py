12b767623e93b24d9c86a86e708e3b71705d5df1
jean sanon <jeansanon180@gmail.com>
2020-06-17 20:23:55 -0400

    Initial commit

---
+++ list-tuple-dict.py
@@ -0,0 +1,20 @@
+"""
+I create a list called 'fruits' using brackets
+I create a tuple called 'pets' using parentheses
+
+"""
+print('\n------------------ List--------------------------')
+
+print('\nlists are not only Changeable but also created using [ ]: ')
+
+fruits = ['apple', 'pineapple', 'mango', 'orange']
+
+print(fruits)
+
+print()
+
+print('------------------ Tuple--------------------------')
+
+pets = ('cat', 'dog', 'ducks')
+
+print(pets)
