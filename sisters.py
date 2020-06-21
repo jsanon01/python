6d1d97d0e84d58ee480ec1e08d1f8760cd2c7597
jean sanon <jeansanon180@gmail.com>
2020-06-21 10:54:21 -0400

    Initial commit

---
+++ sisters.py
@@ -0,0 +1,18 @@
+"""
+
+
+"""
+
+def invisibles():
+    print("\nLes 4 soeurs invisibles:\n- drogue\n- criminalite\n- superstitution\n- corruption ")
+
+def visibles():
+    print("\nLes 4 soeurs visibles:\n- money\n- politics\n- sex\n- religion ")
+
+def puces():
+    print("\nLes 4 puces indomptables:\n- morpions\n- tics\n- punaises\n- carapattes ")
+
+invisibles()
+visibles()
+puces()
+print()
