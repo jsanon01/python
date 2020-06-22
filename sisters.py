57113e76c4a466ce2d67bbaef4060d47dd904152
jean sanon <jeansanon180@gmail.com>
2020-06-22 19:12:47 -0400

    2nd commit

--- sisters.py
+++ sisters.py
@@ -1,16 +1,17 @@
 """
-
+2 of them reside in humans
+2 of them live in animals
 
 """
 
 def invisibles():
-    print("\nLes 4 soeurs invisibles:\n- drogue\n- criminalite\n- superstitution\n- corruption ")
+    print("\nLes 4 soeurs invisibles:\n- drogue\n- criminalite\n- superstitution\n- corruption ".title())
 
 def visibles():
-    print("\nLes 4 soeurs visibles:\n- money\n- politics\n- sex\n- religion ")
+    print("\nLes 4 soeurs visibles:\n- money\n- politics\n- sex\n- religion ".title())
 
 def puces():
-    print("\nLes 4 puces indomptables:\n- morpions\n- tics\n- punaises\n- carapattes ")
+    print("\nLes 4 puces indomptables ou a l'avant-garde (indomitable & upfront fleas):\n- morpions (crabs)\n- tics (tics)\n- punaises (bedbugs)\n- carapattes (carapattes)".title())
 
 invisibles()
 visibles()
