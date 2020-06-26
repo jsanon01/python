2070831824656b5cd570e290d5afd6f54ddaaf66
jean sanon <jeansanon180@gmail.com>
2020-06-26 10:21:34 -0400

    4th commit

--- sisters.py
+++ sisters.py
@@ -11,7 +11,7 @@ def visibles():
     print("\nLes 4 soeurs visibles:\n- money\n- politics\n- sex\n- religion ".title())
 
 def puces():
-    print("\nLes 4 puces indomptables ou a l'avant-garde (indomitable & upfront fleas):\n- morpions (crabs live on human hair)\n- tics (tics live on animals)\n- punaises (bedbugs live on human clothing)\n- carapattes (carapattes ive on animals)".title())
+    print("\nLes 4 puces indomptables ou a l'avant-garde (indomitable & upfront fleas):\n- morpions (crabs live on human hair)\n- tics (tics live on animals)\n- punaises (bedbugs live on human clothing)\n- carapattes (carapattes live on animals)".title())
 
 invisibles()
 visibles()
