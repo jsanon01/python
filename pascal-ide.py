37d724f091c5b1dd9fe441a498d9ac134617941f
jean sanon <jeansanon180@gmail.com>
2020-07-02 15:22:53 -0400

    Initial commit

---
+++ pascal-ide.py
@@ -0,0 +1,30 @@
+"""
+
+
+"""
+
+def pascal():
+    print("pascal ide is\n- a docror of philosophy\n- doctor of medicine\n- master of theology.".title())
+
+def ouvrage():
+    print("here are some of his literary work: ")
+    print("- 'know yourself better to love yourself' means 'mieux se connaitre pour mieux s'aimer")
+    print("- 'working methodically is to succeed' means 'travailler avec methode c'est reussir''")
+    print("- 'building your personality' means 'construire sa personnalite'")
+    print("- 'knowing your heart' means 'connaitre ses blessures'")
+    print("- 'body to heart' means 'le corps a coeur'")
+
+
+def main():
+    x = int(input("\nEnter a number: "))
+    while x:
+        if x == 1:
+            pascal()
+        elif x == 2:
+            ouvrage()
+        else:
+            print("\nInvalid number")
+        x = int(input("\nEnter a number: "))
+    print('\nExiting the loop!')
+
+main()
