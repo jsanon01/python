baad2c6cdb328b2b4738d2359bf7fccd6c56246e
jean sanon <jeansanon180@gmail.com>
2020-07-03 11:47:53 -0400

    Initial commit

---
+++ python-folder/slice.py
@@ -0,0 +1,22 @@
+"""
+
+
+"""
+
+
+print("\nthe slice constructor returns the following syntaxes: ".title())
+#print('\n- slice(stop)\n- slice(start, stop, step)') 
+
+print('\n----------------- Syntax: slice(stop) ----------------------')
+
+print("\nstring = 'astring'")
+
+print('\n- s1 = slice(3) => it means to stop at 3') 
+
+string = 'astring'
+
+s1 = slice(3)
+
+print('\nresult is: '.title(), string[s1])
+
+print()
