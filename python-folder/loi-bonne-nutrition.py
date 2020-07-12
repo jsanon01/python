144c9d364ff8ca46a9036e718737ef8cb80be256
jean sanon <jeansanon180@gmail.com>
2020-07-12 11:02:35 -0400

    Initial commit

---
+++ python-folder/loi-bonne-nutrition.py
@@ -0,0 +1,30 @@
+"""
+
+
+"""
+
+def quantite():
+    print("\nla quantite d'aliments integree chaque jour doit etre\n- suffisante pour couvrir les besoins de l'organisme en energie et a la croissance\n- suffisante a l'entretien des tissus et des organes .")
+    print("une alimentation correcte produit: les fonctions suivantes:\n- croissance, reproduction, travail, activite mentale, stabilite du poids a l'age adulte")
+
+def qualite():
+    print("\nle regime alimentaire doit etre complet et varie dans sa composition, et procurer a l'organisme toutes les substances necessaires:")
+    print("- hydrate de carbone (glucides)\n- graisses (lipides)\n- proteines (protides)\n- vitamines\n- mineraux\n- eau\nfibres generales")
+
+def equilibre():
+    print("\nles quantites des diverses substances doivent garder entre elles un equilibre correct")
+    print("- hydrates de carbone => 55% a 75% du total des calories.\n- graisses => a ne pas exceder 30% des calories (origine vegetale)\n- proteines => 10% a 15% du total des calories")
+
+def ajustement():
+    print("\nle choix, la preparation et la quantite des aliments doivent s'ajuster:\n- au poids\n- a l'age\n- a l'etat physiologique\n- et au type d'activite.")
+
+
+quantite()
+
+qualite()
+
+equilibre()
+
+ajustement()
+
+print()
