a93ea9e7af23b82920f8700f3e396e320614d8f2
jean sanon <jeansanon180@gmail.com>
2020-07-15 19:33:20 -0400

    4th commit

--- python-folder/loi-bonne-nutrition.py
+++ python-folder/loi-bonne-nutrition.py
@@ -8,11 +8,11 @@ def quantite():
     print("- une alimentation correcte produit les fonctions suivantes:\n- croissance, reproduction, travail, activite mentale, stabilite du poids a l'age adulte".title())
 
 def qualite():
-    print("\nle regime alimentaire doit etre complet et varie dans sa composition,\n et procurer a l'organisme toutes les substances necessaires:")
-    print("- hydrate de carbone (glucides)\n- graisses (lipides)\n- proteines (protides)\n- vitamines\n- mineraux\n- eau\n- fibres generales")
+    print("\nle regime alimentaire doit etre complet et varie dans sa composition,\n et procurer a l'organisme toutes les substances necessaires:".title())
+    print("- hydrate de carbone (glucides)\n- graisses (lipides)\n- proteines (protides)\n- vitamines\n- mineraux\n- eau\n- fibres generales".title())
 
 def equilibre():
-    print("\nles quantites des diverses substances doivent garder entre elles un equilibre correct")
+    print("\nles quantites des diverses substances doivent garder entre elles un equilibre correct".title())
     print("- hydrates de carbone => 55% a 75% du total des calories.\n- graisses => a ne pas exceder 30% des calories (origine vegetale)\n- proteines => 10% a 15% du total des calories")
 
 def ajustement():
