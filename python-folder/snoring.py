774e0fb6cfde24564fad55467d552571c48cfb35
jean sanon <jeansanon180@gmail.com>
2020-07-21 19:23:26 -0400

    Initial commit

---
+++ python-folder/snoring.py
@@ -0,0 +1,18 @@
+"""
+
+
+"""
+
+
+def sleep_study():
+    print("in a polysomnography, you are connected to many sensors and observed overnight\nDuring the sleep study, the following information is recorded: ")
+    print("- brain waves\n- blood oxygen level\n- heart rate\n- sleep stages\n- eyes and leg movements")
+
+
+def symptoms():
+    print("- witnessed breathing pauses during sleep\n- difficulty concentrating\n- morning headaches\n- store throat upon awakening\n- restless sleep\n- gasping or chocking at night\n- high blood pressure\n- chest pain at night\n- loud snoring disrupting your partner's sleep")
+
+
+sleep_study()
+print()
+symptoms()
