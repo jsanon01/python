1bef5be8312b38d5647e59da285cb3e51ba6be1e
jean sanon <jeansanon180@gmail.com>
2020-07-22 20:16:14 -0400

    2nd commit

--- python-folder/snoring.py
+++ python-folder/snoring.py
@@ -1,18 +1,21 @@
 """
-
+I want to define a function named sleep_study()
 
 """
-
+print()
 
 def sleep_study():
-    print("in a polysomnography, you are connected to many sensors and observed overnight\nDuring the sleep study, the following information is recorded: ")
-    print("- brain waves\n- blood oxygen level\n- heart rate\n- sleep stages\n- eyes and leg movements")
+    print("in a polysomnography, you are connected to many sensors and observed overnight\nDuring the sleep study, the following information is recorded: ".title())
+    print("- brain waves\n- blood oxygen level\n- heart rate\n- sleep stages\n- eyes and leg movements".title())
 
 
 def symptoms():
-    print("- witnessed breathing pauses during sleep\n- difficulty concentrating\n- morning headaches\n- store throat upon awakening\n- restless sleep\n- gasping or chocking at night\n- high blood pressure\n- chest pain at night\n- loud snoring disrupting your partner's sleep")
-
+    print("- witnessed breathing pauses during sleep\n- difficulty concentrating\n- morning headaches\n- store throat upon awakening\n- restless sleep\n- gasping or chocking at night\n- high blood pressure\n- chest pain at night\n- loud snoring disrupting your partner's sleep".title())
 
 sleep_study()
+
 print()
+
 symptoms()
+
+print()
