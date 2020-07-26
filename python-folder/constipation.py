4f4dabf6d71350b4c1de2e8e1e253c4734f0201c
jean sanon <jeansanon180@gmail.com>
2020-07-26 17:32:49 -0400

    Initial commit

---
+++ python-folder/constipation.py
@@ -0,0 +1,21 @@
+"""
+p94 la sante par les plantes
+
+"""
+print()
+
+def constipation():
+    print("rarete de l'evacuation des selles, due a une retention\nanormalement prolongee des matieres fecales dans l'intestin...")
+
+def causes():
+    print("les principales causes de la constipation sont:\n- fonctionelles\n- dietetiques\n- manque d'xercise")
+
+def traitement():
+    print("- dartier\n- ble\n- sene\n- tamarin")
+
+constipation()
+print()
+causes()
+print()
+traitement()
+print()
