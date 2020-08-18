9ea5779b01b6b49647aa5c8ba8f60b974fe614b1
jean sanon <jeansanon180@gmail.com>
2020-08-18 19:19:56 -0400

    1st commit

---
+++ python-folder/decisive-factors.py
@@ -0,0 +1,40 @@
+"""
+
+
+"""
+
+def alimentation():
+    print('alimentation')
+
+def air():
+    print('air')
+
+def eau():
+    print('eau')
+
+def soleil():
+    print('soleil')
+
+def exercise():
+    print('exercise physique')
+
+def repos():
+    print('repos')
+
+def abstinence():
+    print('abstinence de toxiques')
+
+def positif():
+    print("etat d'esprit positif")
+
+def main():
+    alimentation()
+    air()
+    eau()
+    soleil()
+    exercise()
+    repos()
+    abstinence()
+    positif()
+
+main()
