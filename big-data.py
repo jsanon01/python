37d9e2a0bb44ceb5c6c2f4da57bbd23a0b7d107a
jean sanon <jeansanon180@gmail.com>
2020-08-21 09:37:55 -0400

    8th commit

--- big-data.py
+++ big-data.py
@@ -8,7 +8,7 @@ def big_data():
     print('\nbig data deals with collection, storage, processing and analysis of\nmassive-scale data.\n\nbig data has the potential to power next generation of smart applications.'.title())
 
 def characteristics():
-    print('\nbig data characteristics are: the 5vs.\nvolume | velocity | variety | veracity | variety | value'.title())
+    print('\nbig data characteristics are: the 5vs.\nvolume | velocity | variety | veracity | value'.title())
 
 def examples():
     print('\nvarious applications used by big data.'.title())
