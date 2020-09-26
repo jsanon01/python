45a13591c2e241409f5e6966f6df1e4f9f361b4e
jean sanon <jeansanon180@gmail.com>
2020-09-25 23:07:44 -0400

    2nd commit

--- unix-philosophy.py
+++ unix-philosophy.py
@@ -1,12 +1,13 @@
 
 
-
-
-print("Do one thing & Do it well".title())
-print("everything is file".title())
-print("small is beautiful".title())
-print("store data & configuration in flat text files".title())
-print("use shell script to increase leverage & portability".title())
-print("chain programs together to complete complex tasks".title())
-print("choose portability over efficiency".title())
-print("keep it simple, stupid".title())
+print()
+print('---------- UNIX/Linux Philosophy ----------')
+print("\nDo one thing & Do it well".title())
+print("\neverything is file".title())
+print("\nsmall is beautiful".title())
+print("\nstore data & configuration in flat text files".title())
+print("\nuse shell script to increase leverage & portability".title())
+print("\nchain programs together to complete complex tasks".title())
+print("\nchoose portability over efficiency".title())
+print("\nkeep it simple, stupid".title())
+print()
