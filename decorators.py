6925d479c6182e870b16983389cae9fde8d9e6bd
jsanon01 <jeansanon180@gmail.com>
2022-04-03 16:07:16 -0400

    2nd commit

--- decorators.py
+++ decorators.py
@@ -12,13 +12,21 @@ hi_func = outer_func('Hi is the outer function')
 bye_func = outer_func('Bye is the inner function')
 
 print('-------------------------------------------------------------')
+
 print('A decorator is a function taking another one as argument...')
 
 hi_func()
+
 print()
+
 print('def outer_function:')
+
 print('    def inner_function:')
+
 print()
+
 bye_func()
+
 print()
+
 print('-------------------------------------------------------------')
