ddeb588d8b1c3bf49a03f361f2e8795f7a277a8a
jsanon01 <jeansanon180@gmail.com>
2022-04-04 19:07:37 -0400

    script has been modified

--- presidents.py
+++ presidents.py
@@ -1,9 +1,15 @@
 # List of 20th and 21st century US Presidents
+print()
+
+print('This script displays few presidents in 20th and 21st century:')
+
+print()
 
 presidents = ['jfk', 'clinton', 'obama', 'trump']
 
 for president in presidents:
-    print(f'{president.title()} was elected US President!')
+    print(f'{president.title()} was elected US President!\n')
 
 print('They are all Americans-born!')
 
+print()
