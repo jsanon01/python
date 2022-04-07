5abebe87665a6278e29233555e72e9d1d457dc77
jsanon01 <jeansanon180@gmail.com>
2022-04-06 20:42:47 -0400

    1st commit

---
+++ hour.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# hour calculation
+
+hour = int(input('Enter hour: '))
+
+if hour >= 1 and hour < 12:
+    print('Good morning')
+elif hour >= 12 and hour < 18:
+    print('Good afternoon')
+elif hour >= 18 and hour < 24:
+    print('Good evening')
+else:
+    print('Wrong format | More than 24-hour!')
